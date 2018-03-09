a697b8315d80b5adf3624a8110c18876652004b8
Awilum <awilum@msn.com>
2018-03-10 01:44:42 +0300

    Pages class refactoring

--- rawilum/Pages.php
+++ rawilum/Pages.php
@@ -2,6 +2,9 @@
 namespace Rawilum;
 
 use Arr;
+use Url;
+use Response;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This file is part of the Rawilum.
@@ -12,12 +15,14 @@ use Arr;
  * file that was distributed with this source code.
  */
 
-class Pages extends Page
+class Pages
 {
     /**
      * @var Rawilum
      */
     protected $rawilum;
+
+    public $page;
 
     /**
      * Construct
@@ -25,6 +30,102 @@ class Pages extends Page
     public function __construct(Rawilum $c)
     {
         $this->rawilum = $c;
+    }
+
+    /**
+     * Get page
+     */
+    public function getPage($url = '', $raw = false, $url_abs = false)
+    {
+
+        $file = $this->finder($url, $url_abs);
+
+        if ($raw) {
+            $page = trim(file_get_contents($file));
+            $this->page = $page;
+            $this->rawilum['events']->dispatch('onPageContentRawAfter');
+        } else {
+            $page = $this->parse($file);
+            $this->page = $page;
+            $this->page['content'] = $this->rawilum['filters']->dispatch('content', $this->rawilum['markdown']->text($this->page['content']));
+            $this->rawilum['events']->dispatch('onPageContentAfter');
+        }
+
+        return $this->page;
+    }
+
+    /**
+     * Page finder
+     */
+    public function finder($url = '', $url_abs = false)
+    {
+
+        // If url is empty that its a homepage
+        if ($url_abs) {
+          if ($url) {
+              $file = $url;
+          } else {
+              $file = CONTENT_PATH . '/pages/' . $this->rawilum['config']->get('site.pages.main') . '/' . 'index.md';
+          }
+        } else {
+          if ($url) {
+              $file = CONTENT_PATH . '/pages/' . $url . '/index.md';
+          } else {
+              $file = CONTENT_PATH . '/pages/' . $this->rawilum['config']->get('site.pages.main') . '/' . 'index.md';
+          }
+        }
+
+        // Get 404 page if file not exists
+        if ($this->rawilum['filesystem']->exists($file)) {
+            $file = $file;
+        } else {
+            $file = CONTENT_PATH . '/pages/404/index.md';
+            Response::status(404);
+        }
+
+        return $file;
+    }
+
+    /**
+     * Render page
+     */
+    public function renderPage($page)
+    {
+        $template_ext  = '.php';
+        $template_name = empty($page['template']) ? 'index' : $page['template'];
+        $site_theme    = $this->rawilum['config']->get('site.theme');
+        $template_path = THEMES_PATH . '/' . $site_theme . '/' . $template_name . $template_ext;
+
+        if ($this->rawilum['filesystem']->exists($template_path)) {
+            include $template_path;
+        } else {
+            throw new RuntimeException("Rawilum site config file does not exist.");
+        }
+    }
+
+    /**
+     * Page parser
+     */
+    public function parse($file)
+    {
+        $page = trim(file_get_contents($file));
+
+        $page = explode('---', $page, 3);
+
+        $frontmatter = Yaml::parse($page[1]);
+        $content = $page[2];
+
+        // @TODO fix this!
+        $url = str_replace(CONTENT_PATH . '/pages', Url::getBase(), $file);
+        $url = str_replace('index.md', '', $url);
+        $url = str_replace('.md', '', $url);
+        $url = str_replace('\\', '/', $url);
+        $url = rtrim($url, '/');
+
+        $frontmatter['url']  = $url;
+        $frontmatter['slug'] = basename($file, '.md');
+
+        return ['frontmatter' => $frontmatter, 'content' => $content];
     }
 
     /**
