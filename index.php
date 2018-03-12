59bd61e24595fb9030c1d503c7f1bacc3dd2b463
Awilum <awilum@msn.com>
2018-03-13 01:00:10 +0300

    Added new constant - RAWILUM_MINIMUM_PHP #5

--- index.php
+++ index.php
@@ -10,8 +10,13 @@
 
 namespace Rawilum;
 
+/**
+ * Define the application's minimum supported PHP version.
+ */
+define('RAWILUM_MINIMUM_PHP', '7.1.3');
+
 // Check PHP Version
-version_compare($ver = PHP_VERSION, $req = '7.1.3', '<') and exit(sprintf('You are running PHP %s, but Rawilum needs at least <strong>PHP %s</strong> to run.', $ver, $req));
+version_compare($ver = PHP_VERSION, $req = RAWILUM_MINIMUM_PHP, '<') and exit(sprintf('You are running PHP %s, but Rawilum needs at least <strong>PHP %s</strong> to run.', $ver, $req));
 
 // Ensure vendor libraries exist and Register The Auto Loader
 !is_file($autoload = __DIR__ . '/vendor/autoload.php') and exit("Please run: <i>composer install</i>");
