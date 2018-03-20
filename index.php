d7746a24616e26308c1e28ad65503d6de06437e3
Awilum <awilum@msn.com>
2018-03-21 00:28:34 +0300

    NEW BRAND NAME - FLEXTYPE

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
  */
 
 // Define the application minimum supported PHP version.
-define('RAWILUM_MINIMUM_PHP', '7.1.3');
+define('FLEXTYPE_MINIMUM_PHP', '7.1.3');
 
 // Check PHP Version
-version_compare($ver = PHP_VERSION, $req = RAWILUM_MINIMUM_PHP, '<') and exit(sprintf('You are running PHP %s, but Flextype needs at least <strong>PHP %s</strong> to run.', $ver, $req));
+version_compare($ver = PHP_VERSION, $req = FLEXTYPE_MINIMUM_PHP, '<') and exit(sprintf('You are running PHP %s, but Flextype needs at least <strong>PHP %s</strong> to run.', $ver, $req));
 
 // Ensure vendor libraries exist and Register The Auto Loader
 !is_file($autoload = __DIR__ . '/vendor/autoload.php') and exit("Please run: <i>composer install</i>");
