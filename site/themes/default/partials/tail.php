dbf883affc6f58ea4677dbb89b8920f4ff6093e0
Awilum <awilum@msn.com>
2018-04-07 12:24:28 +0300

    Using Flextype Components

--- site/themes/default/partials/tail.php
+++ site/themes/default/partials/tail.php
@@ -1,3 +1,3 @@
-<script src="<?php echo Url::getBase(); ?>/site/themes/<?php echo Flextype\Config::get('site.theme'); ?>/node_modules/jquery/dist/jquery.slim.min.js"></script>
-<script src="<?php echo Url::getBase(); ?>/site/themes/<?php echo Flextype\Config::get('site.theme'); ?>/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?php echo Flextype\Component\Url\Url::getBase(); ?>/site/themes/<?php echo Flextype\Config::get('site.theme'); ?>/node_modules/jquery/dist/jquery.slim.min.js"></script>
+<script src="<?php echo Flextype\Component\Url\Url::getBase(); ?>/site/themes/<?php echo Flextype\Config::get('site.theme'); ?>/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 <?php Flextype\Events::dispatch('onThemeFooter'); ?>
