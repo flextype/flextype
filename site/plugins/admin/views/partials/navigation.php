7dfc89489b1842b78225d9396e0040a6d01ac3f0
Awilum <awilum@msn.com>
2018-04-22 15:40:51 +0300

    Admin: theme fixes

--- site/plugins/admin/views/partials/navigation.php
+++ site/plugins/admin/views/partials/navigation.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom box-shadow">
 <div class="container">
-  <a class="navbar-brand" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>"><?php echo Flextype\Config::get('site.title'); ?></a>
+  <a class="navbar-brand" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/admin"><?php echo Flextype\Config::get('site.title'); ?></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
--- site/plugins/admin/views/partials/tail.php
+++ site/plugins/admin/views/partials/tail.php
@@ -1,5 +1,5 @@
-<script src="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/site/themes/<?php echo Flextype\Config::get('site.theme'); ?>/node_modules/jquery/dist/jquery.slim.min.js"></script>
-<script src="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/site/themes/<?php echo Flextype\Config::get('site.theme'); ?>/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/site/plugins/admin/node_modules/jquery/dist/jquery.slim.min.js"></script>
+<script src="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/site/plugins/admin/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
 <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
 <script src="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/site/plugins/admin/node_modules/codemirror/lib/codemirror.js"></script>
