f9c8197187b8e694c4c2242c38d226b15bedd924
Awilum <awilum@msn.com>
2018-05-16 10:03:41 +0300

    Simple theme updates

--- site/themes/simple/views/templates/default.php
+++ site/themes/simple/views/templates/default.php
@@ -1,4 +1,4 @@
 <?php namespace Flextype; ?>
-<?php Themes::template('partials/head')->display(); ?>
+<?php Themes::view('partials/head')->display(); ?>
 <?php echo $page['content']; ?>
-<?php Themes::template('partials/footer')->display(); ?>
+<?php Themes::view('partials/footer')->display(); ?>
