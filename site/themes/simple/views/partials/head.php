bc8fc0ef7401b588eb3e65832ae7eeeb5a032405
Awilum <awilum@msn.com>
2018-06-08 01:35:19 +0300

    Simple Theme: Cross-site scripting Vulnerabilities fixes

--- site/themes/simple/views/partials/head.php
+++ site/themes/simple/views/partials/head.php
@@ -1,20 +1,20 @@
 <?php
     namespace Flextype;
-    use Flextype\Component\{Event\Event, Http\Http, Registry\Registry, Assets\Assets, Text\Text};
+    use Flextype\Component\{Event\Event, Http\Http, Registry\Registry, Assets\Assets, Text\Text, Html\Html};
 ?>
 <!doctype html>
 <html lang="<?php echo Registry::get('site.locale'); ?>">
   <head>
     <meta charset="<?php echo Text::lowercase(Registry::get('site.charset')); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="<?php echo (isset($page['description']) ? $page['description'] : Registry::get('site.description')); ?>">
+    <meta name="description" content="<?php echo (isset($page['description']) ? Html::toText($page['description']) : Html::toText(Registry::get('site.description'))); ?>">
     <meta name="keywords" content="<?php echo (isset($page['keywords']) ? $page['keywords'] : Registry::get('site.keywords')); ?>">
     <meta name="robots" content="<?php echo (isset($page['robots']) ? $page['robots'] : Registry::get('site.robots')); ?>">
     <meta name="generator" content="Powered by Flextype <?php echo Flextype::VERSION; ?>" />
 
 	<?php Event::dispatch('onThemeMeta'); ?>
 
-	<title><?php echo $page['title']; ?> | <?php echo Registry::get('site.title'); ?></title>
+	<title><?php echo Html::toText($page['title']); ?> | <?php echo Html::toText(Registry::get('site.title')); ?></title>
 
     <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700' rel='stylesheet' type='text/css'>
 
