dc4a10fb4ae5ff3a0cebaac7209b35ffd48d6240
Awilum <awilum@msn.com>
2018-12-15 02:28:47 +0300

    Update theme simple according to the php template syntax guidelines

--- site/themes/simple/views/templates/default.php
+++ site/themes/simple/views/templates/default.php
@@ -1,4 +1,4 @@
 <?php namespace Flextype; ?>
 <?php Themes::view('partials/head')->display(); ?>
-<?= $page['content']; ?>
+<?= $page['content'] ?>
 <?php Themes::view('partials/footer')->display(); ?>
