cfad62274d50c49e73232f5af6516298dee0d0a1
Awilum <awilum@msn.com>
2018-12-23 23:44:23 +0300

    Snippets: Implementation

---
+++ flextype/Snippets.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @package Flextype
+ *
+ * @author Sergey Romanenko <awilum@yandex.ru>
+ * @link http://flextype.org
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flextype;
+
+use Flextype\Component\Filesystem\Filesystem;
+use Flextype\Component\Event\Event;
+use Flextype\Component\I18n\I18n;
+use Flextype\Component\Registry\Registry;
+
+class Snippets
+{
+
+}
