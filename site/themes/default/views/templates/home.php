fa3b48117cc0af0aef1bcea97dcc1c0feed5d036
Awilum <awilum@msn.com>
2018-12-26 02:59:00 +0300

    Default theme: home template updates

--- site/themes/default/views/templates/home.php
+++ site/themes/default/views/templates/home.php
@@ -1,7 +1,11 @@
 <?php namespace Flextype ?>
 <?php Themes::view('partials/head')->display() ?>
 <?= $entry['content'] ?>
-<?php foreach (Entries::getEntries('blog') as $post): ?>
-    <?= $post['title'] ?>
+<?php foreach (Entries::getEntries('blog', false, 'date', 'DESC', 0, 3) as $entry): ?>
+    <a href="<?= $entry['url'] ?>">
+        <h3><?= $entry['title'] ?></h3>
+        <?= $entry['summary'] ?>
+    </a>
 <?php endforeach ?>
+READ THE REST OF THE BLOG
 <?php Themes::view('partials/footer')->display() ?>
