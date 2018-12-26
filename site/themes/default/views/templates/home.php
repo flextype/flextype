f1469d419ed56e90e65ed8ab0e6effdd00280914
Awilum <awilum@msn.com>
2018-12-26 03:48:59 +0300

    Default theme: updates

--- site/themes/default/views/templates/home.php
+++ site/themes/default/views/templates/home.php
@@ -1,11 +1,12 @@
 <?php namespace Flextype ?>
 <?php Themes::view('partials/head')->display() ?>
 <?= $entry['content'] ?>
-<?php foreach (Entries::getEntries('blog', false, 'date', 'DESC', 0, 3) as $entry): ?>
-    <a href="<?= $entry['url'] ?>">
+<?php foreach (Entries::getEntries('blog', 'date', 'DESC', 0, 3) as $entry): ?>
+    <a href="<?= $entry['url'] ?>" class="blog-post">
         <h3><?= $entry['title'] ?></h3>
-        <?= $entry['summary'] ?>
+        <p><?= $entry['summary'] ?></p>
+        <div><?= $entry['date'] ?></div>
     </a>
 <?php endforeach ?>
-READ THE REST OF THE BLOG
+<a href="#">Read the rest of the blog</a>
 <?php Themes::view('partials/footer')->display() ?>
