149baa39938f068408908885c57c70b4b0e28779
Awilum <awilum@msn.com>
2018-12-28 12:54:10 +0300

    Admin Panel: Fix issues with templates and partials list.

--- site/plugins/admin/views/templates/extends/templates/list.php
+++ site/plugins/admin/views/templates/extends/templates/list.php
@@ -21,7 +21,7 @@
 ?>
 <?php Themes::view('admin/views/partials/content-start')->display() ?>
 
-<?php if (count($templates_list) > 0): ?>
+<?php if (count($templates_list) > 0 || count($partials_list) > 0): ?>
 <table class="table no-margin">
     <thead>
         <tr>
@@ -31,48 +31,52 @@
         </tr>
     </thead>
     <tbody>
-        <?php foreach ($templates_list as $template): ?>
-        <tr>
-            <td>
-                <?= $template ?>
-            </td>
-            <td><?= Text::lowercase(__('admin_template')) ?></td>
-            <td class="text-right">
-                <div class="btn-group">
-                  <a class="btn btn-default" href="<?= Http::getBaseUrl() ?>/admin/templates/edit?template=<?= $template ?>"><?= __('admin_edit') ?></a>
-                  <button type="button" class="btn btn-default dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class="sr-only">Toggle Dropdown</span>
-                  </button>
-                  <div class="dropdown-menu">
-                    <a class="dropdown-item" href="<?= Http::getBaseUrl() ?>/admin/templates/rename?template=<?= $template ?>"><?= __('admin_rename') ?></a>
-                    <a class="dropdown-item" href="<?= Http::getBaseUrl() ?>/admin/templates/duplicate?template=<?= $template ?>&token=<?= Token::generate() ?>"><?= __('admin_duplicate') ?></a>
-                  </div>
-                </div>
-                <a class="btn btn-default" href="<?= Http::getBaseUrl() ?>/admin/templates/delete?template=<?= $template ?>&token=<?= Token::generate() ?>"><?= __('admin_delete') ?></a>
-            </td>
-        </tr>
-        <?php endforeach ?>
-        <?php foreach ($partials_list as $partial): ?>
-        <tr>
-            <td>
-                <?= $partial ?>
-            </td>
-            <td><?= Text::lowercase(__('admin_partial')) ?></td>
-            <td class="text-right">
-                <div class="btn-group">
-                  <a class="btn btn-default" href="<?= Http::getBaseUrl() ?>/admin/templates/edit?template=<?= $partial ?>&type=partial"><?= __('admin_edit') ?></a>
-                  <button type="button" class="btn btn-default dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class="sr-only">Toggle Dropdown</span>
-                  </button>
-                  <div class="dropdown-menu">
-                    <a class="dropdown-item" href="<?= Http::getBaseUrl() ?>/admin/templates/rename?template=<?= $partial ?>&type=partial"><?= __('admin_rename') ?></a>
-                    <a class="dropdown-item" href="<?= Http::getBaseUrl() ?>/admin/templates/duplicate?template=<?= $partial ?>&type=partial&token=<?= Token::generate() ?>"><?= __('admin_duplicate') ?></a>
-                  </div>
-                </div>
-                <a class="btn btn-default" href="<?= Http::getBaseUrl() ?>/admin/templates/delete?template=<?= $partial ?>&type=partial&token=<?= Token::generate() ?>"><?= __('admin_delete') ?></a>
-            </td>
-        </tr>
-        <?php endforeach ?>
+        <?php if (count($templates_list) > 0): ?>
+            <?php foreach ($templates_list as $template): ?>
+            <tr>
+                <td>
+                    <?= $template ?>
+                </td>
+                <td><?= Text::lowercase(__('admin_template')) ?></td>
+                <td class="text-right">
+                    <div class="btn-group">
+                      <a class="btn btn-default" href="<?= Http::getBaseUrl() ?>/admin/templates/edit?template=<?= $template ?>"><?= __('admin_edit') ?></a>
+                      <button type="button" class="btn btn-default dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="sr-only">Toggle Dropdown</span>
+                      </button>
+                      <div class="dropdown-menu">
+                        <a class="dropdown-item" href="<?= Http::getBaseUrl() ?>/admin/templates/rename?template=<?= $template ?>"><?= __('admin_rename') ?></a>
+                        <a class="dropdown-item" href="<?= Http::getBaseUrl() ?>/admin/templates/duplicate?template=<?= $template ?>&token=<?= Token::generate() ?>"><?= __('admin_duplicate') ?></a>
+                      </div>
+                    </div>
+                    <a class="btn btn-default" href="<?= Http::getBaseUrl() ?>/admin/templates/delete?template=<?= $template ?>&token=<?= Token::generate() ?>"><?= __('admin_delete') ?></a>
+                </td>
+            </tr>
+            <?php endforeach ?>
+        <?php endif ?>
+        <?php if (count($partials_list) > 0): ?>
+            <?php foreach ($partials_list as $partial): ?>
+            <tr>
+                <td>
+                    <?= $partial ?>
+                </td>
+                <td><?= Text::lowercase(__('admin_partial')) ?></td>
+                <td class="text-right">
+                    <div class="btn-group">
+                      <a class="btn btn-default" href="<?= Http::getBaseUrl() ?>/admin/templates/edit?template=<?= $partial ?>&type=partial"><?= __('admin_edit') ?></a>
+                      <button type="button" class="btn btn-default dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="sr-only">Toggle Dropdown</span>
+                      </button>
+                      <div class="dropdown-menu">
+                        <a class="dropdown-item" href="<?= Http::getBaseUrl() ?>/admin/templates/rename?template=<?= $partial ?>&type=partial"><?= __('admin_rename') ?></a>
+                        <a class="dropdown-item" href="<?= Http::getBaseUrl() ?>/admin/templates/duplicate?template=<?= $partial ?>&type=partial&token=<?= Token::generate() ?>"><?= __('admin_duplicate') ?></a>
+                      </div>
+                    </div>
+                    <a class="btn btn-default" href="<?= Http::getBaseUrl() ?>/admin/templates/delete?template=<?= $partial ?>&type=partial&token=<?= Token::generate() ?>"><?= __('admin_delete') ?></a>
+                </td>
+            </tr>
+            <?php endforeach ?>
+        <?php endif ?>
     </tbody>
 </table>
 <?php else: ?>
