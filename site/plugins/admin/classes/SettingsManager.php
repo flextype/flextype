b2e933ac0d15b425871e9219313f9ed819fa505b
Awilum <awilum@msn.com>
2019-02-02 02:47:14 +0300

    Admin Panel: Settings Manager - code fixes and refactoring according to Scrutinizer Tests.

--- site/plugins/admin/classes/SettingsManager.php
+++ site/plugins/admin/classes/SettingsManager.php
@@ -3,19 +3,13 @@
 namespace Flextype;
 
 use Flextype\Component\Arr\Arr;
-use Flextype\Component\Number\Number;
 use Flextype\Component\I18n\I18n;
 use Flextype\Component\Http\Http;
-use Flextype\Component\Event\Event;
 use Flextype\Component\Filesystem\Filesystem;
-use Flextype\Component\Session\Session;
 use Flextype\Component\Registry\Registry;
 use Flextype\Component\Token\Token;
-use Flextype\Component\Text\Text;
-use Flextype\Component\Form\Form;
 use Flextype\Component\Notification\Notification;
 use function Flextype\Component\I18n\__;
-use Gajus\Dindent\Indenter;
 
 class SettingsManager
 {
