71ad42d51256c9fdc0b8f7d3b7c9ff20e2d0f781
Awilum <awilum@msn.com>
2019-02-02 01:48:37 +0300

    Admin Panel: Admin - code fixes and refactoring according to Scrutinizer Tests.

--- site/plugins/admin/admin.php
+++ site/plugins/admin/admin.php
@@ -13,21 +13,11 @@ namespace Flextype;
  * file that was distributed with this source code.
  */
 
-use Flextype\Component\Arr\Arr;
-use Flextype\Component\Number\Number;
 use Flextype\Component\I18n\I18n;
 use Flextype\Component\Http\Http;
 use Flextype\Component\Event\Event;
-use Flextype\Component\Filesystem\Filesystem;
-use Flextype\Component\Session\Session;
 use Flextype\Component\Registry\Registry;
-use Flextype\Component\Token\Token;
-use Flextype\Component\Text\Text;
-use Flextype\Component\Notification\Notification;
-use Flextype\Component\Form\Form;
 use function Flextype\Component\I18n\__;
-use Symfony\Component\Yaml\Yaml;
-use Gajus\Dindent\Indenter;
 
 //
 // If isAdminArea
