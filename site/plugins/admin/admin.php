eae0f6802b5f4c5b9148574612873a7de0b6a40d
Awilum <awilum@msn.com>
2019-02-02 01:52:16 +0300

    Admin Panel: Admin - code fixes and refactoring according to Scrutinizer Tests.

--- site/plugins/admin/admin.php
+++ site/plugins/admin/admin.php
@@ -17,6 +17,7 @@ use Flextype\Component\I18n\I18n;
 use Flextype\Component\Http\Http;
 use Flextype\Component\Event\Event;
 use Flextype\Component\Registry\Registry;
+use Flextype\Component\Notification\Notification;
 use function Flextype\Component\I18n\__;
 
 //
