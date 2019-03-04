00327eaf61648d663939c8f7b1107e3755394384
Awilum <awilum@msn.com>
2019-03-04 16:46:15 +0300

    Flextype Slim Integration - next round of integration

--- site/themes/default/templates/templates/about.php
+++
@@ -1,6 +0,0 @@
-<?php namespace Flextype ?>
-<?php Themes::view('partials/head')->display() ?>
-<h1><?= $entry['title'] ?></h1>
-<?= $entry['content'] ?>
-<img src="<?= Images::getImageUrl($entry['slug'] . '/' . $entry['image'], ['w' => '670', 'dpr' => '2']) ?>">
-<?php Themes::view('partials/footer')->display() ?>
