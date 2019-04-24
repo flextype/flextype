4d64c735914e827b565fd46bda4d928925bc7fb4
Awilum <awilum@msn.com>
2019-04-24 21:54:32 +0300

    Flextype Box Plugin: Admin #125 #117 - Plugins Controller implementation

---
+++ site/plugins/admin/app/Controllers/PluginsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Flextype;
+
+use Flextype\Component\Filesystem\Filesystem;
+use Flextype\Component\Date\Date;
+use Flextype\Component\Arr\Arr;
+use Flextype\Component\Registry\Registry;
+use function Flextype\Component\I18n\__;
+
+class  PluginsController extends Controller
+{
+    public function index($request, $response, $args)
+    {
+
+    }
+
+    public function update($request, $response, $args)
+    {
+
+    }
+
+}
