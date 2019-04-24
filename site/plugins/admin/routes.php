23400afd720673acf1904400d42699f57d0f84b2
Awilum <awilum@msn.com>
2019-04-24 22:26:38 +0300

    Flextype Box Plugin: Admin #125 #117 - Plugins Controller implementation

--- site/plugins/admin/routes.php
+++ site/plugins/admin/routes.php
@@ -9,3 +9,6 @@ $app->get('/admin/information', 'InformationController:index')->setName('admin.i
 $app->get('/admin/settings', 'SettingsController:index')->setName('admin.settings.index');
 $app->post('/admin/settings', 'SettingsController:update')->setName('admin.settings.update');
 $app->post('/admin/settings/clear-cache', 'SettingsController:clearCache')->setName('admin.settings.clear-cache');
+
+// Plugins Controller
+$app->post('/admin/plugins', 'PluginsController:index')->setName('admin.plugins')->setName('admin.plugins.index');
