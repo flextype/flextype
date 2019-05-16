1c088da7bdcad55c748fd19f54a86cb3cbd0b799
Awilum <awilum@msn.com>
2019-05-16 14:08:18 +0300

    Flextype Box Plugin: Admin #125 #117 - Entries Controller/Views implementation

--- site/plugins/admin/routes.php
+++ site/plugins/admin/routes.php
@@ -18,7 +18,10 @@ $app->post('/admin/plugins/change-status', 'PluginsController:changeStatus')->se
 $app->get('/admin/entries', 'EntriesController:index')->setName('admin.entries.index');
 $app->get('/admin/entries/edit', 'EntriesController:index')->setName('admin.entries.edit');
 $app->get('/admin/entries/add', 'EntriesController:add')->setName('admin.entries.add');
-$app->get('/admin/entries/move', 'EntriesController:index')->setName('admin.entries.move');
-$app->get('/admin/entries/rename', 'EntriesController:index')->setName('admin.entries.rename');
-$app->get('/admin/entries/type', 'EntriesController:index')->setName('admin.entries.type');
+$app->post('/admin/entries/add', 'EntriesController:addProcess')->setName('admin.entries.addProcess');
+$app->get('/admin/entries/move', 'EntriesController:move')->setName('admin.entries.move');
+$app->get('/admin/entries/rename', 'EntriesController:rename')->setName('admin.entries.rename');
+$app->post('/admin/entries/rename', 'EntriesController:renameProcess')->setName('admin.entries.renameProcess');
+$app->get('/admin/entries/type', 'EntriesController:type')->setName('admin.entries.type');
+$app->post('/admin/entries/type', 'EntriesController:typeProcess')->setName('admin.entries.typeProcess');
 $app->get('/admin/entries/duplicate', 'EntriesController:index')->setName('admin.entries.duplicate');
