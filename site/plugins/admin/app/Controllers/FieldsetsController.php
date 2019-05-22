fad88e15b7d8bfa6973e487bdcc0f78795d6345c
Awilum <awilum@msn.com>
2019-05-22 15:50:15 +0300

    Flextype Box Plugin: Admin #125 #117 - Fieldsets Controller/Views implementation

---
+++ site/plugins/admin/app/Controllers/FieldsetsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Flextype;
+
+use Flextype\Component\Registry\Registry;
+use function Flextype\Component\I18n\__;
+
+class FieldsetsController extends Controller
+{
+   public function index($request, $response, $args)
+   {
+       return 'sd';
+   }
+}
