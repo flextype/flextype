cb943002617927254f460f733649ed235ed52fd4
Awilum <awilum@msn.com>
2019-05-24 10:37:28 +0300

    Flextype Box Plugin: Admin #125 #117 - Fieldsets Controller/Views implementation

--- site/plugins/admin/app/Controllers/FieldsetsController.php
+++ site/plugins/admin/app/Controllers/FieldsetsController.php
@@ -9,6 +9,17 @@ class FieldsetsController extends Controller
 {
    public function index($request, $response, $args)
    {
-       return 'sd';
+       return $this->view->render($response,
+                                  'plugins/admin/views/templates/extends/fieldsets/index.html', [
+           'menu_item' => 'fieldsets',
+           'fieldsets_list' => $this->fieldsets->fetchList(),
+           'links' =>  [
+                            'fieldsets' => [
+                                'link' => $this->router->urlFor('admin.fieldsets.index'),
+                                'title' => __('admin_fieldsets'),
+                                'attributes' => ['class' => 'navbar-item active']
+                            ],
+                        ]
+       ]);
    }
 }
