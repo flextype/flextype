48d22235aef3091e80322554cfecfaab08c95879
Awilum <awilum@msn.com>
2019-05-24 11:01:12 +0300

    Flextype Box Plugin: Admin #125 #117 - Templates Controller/Views implementation

---
+++ site/plugins/admin/app/Controllers/TemplatesController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Flextype;
+
+use function Flextype\Component\I18n\__;
+
+class TemplatesController extends Controller
+{
+   public function index($request, $response, $args)
+   {
+
+   }
+
+   public function add($request, $response, $args)
+   {
+
+   }
+
+   public function addProcess($request, $response, $args)
+   {
+
+   }
+
+   public function edit($request, $response, $args)
+   {
+
+   }
+
+   public function editProcess($request, $response, $args)
+   {
+
+   }
+
+   public function rename($request, $response, $args)
+   {
+
+   }
+
+   public function renameProcess($request, $response, $args)
+   {
+
+   }
+
+   public function deleteProcess($request, $response, $args)
+   {
+
+   }
+
+   public function duplicateProcess($request, $response, $args)
+   {
+
+   }
+}
