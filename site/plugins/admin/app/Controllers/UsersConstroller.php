5f0ff929283417cca289f92f437dd07af91e5a3b
Awilum <awilum@msn.com>
2019-05-25 21:37:37 +0300

    Flextype Box Plugin: Admin #125 #117 - Users Controller/Views implementation

---
+++ site/plugins/admin/app/Controllers/UsersConstroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Flextype;
+
+use function Flextype\Component\I18n\__;
+
+class UsersController extends Controller
+{
+   public function index($request, $response, $args)
+   {
+       
+   }
+}
