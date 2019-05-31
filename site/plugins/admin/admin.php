5adc20d42f67b9bc4541ef4be7de1e24b6e94741
Awilum <awilum@msn.com>
2019-05-31 17:13:39 +0300

    Flextype Box Plugin: Admin #125 #117 - Navigation links added

--- site/plugins/admin/admin.php
+++ site/plugins/admin/admin.php
@@ -16,6 +16,7 @@ namespace Flextype;
 use Flextype\Component\Registry\Registry;
 use Flextype\Component\I18n\I18n;
 use Flextype\Component\Arr\Arr;
+use function Flextype\Component\I18n\__;
 
 $uri = explode('/', \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER))->getPath());
 
@@ -31,6 +32,14 @@ if (isset($uri) && isset($uri[0]) && $uri[0] == 'admin') {
 
     // Set Default Admin locale
     I18n::$locale = $flextype->registry->get('settings.locale');
+
+    $flextype->registry->set('admin_navigation.content.entries', ['title' => '<i class="far fa-newspaper"></i>' . __('admin_entries'), 'link' => $flextype->router->urlFor('admin.entries.index'), 'attributes' => ['class' => 'nav-link']]);
+    $flextype->registry->set('admin_navigation.extends.fieldsets', ['title' => '<i class="fas fa-list"></i>' . __('admin_fieldsets'), 'link' => $flextype->router->urlFor('admin.fieldsets.index'), 'attributes' => ['class' => 'nav-link']]);
+    $flextype->registry->set('admin_navigation.extends.templates', ['title' => '<i class="fas fa-layer-group"></i>' . __('admin_templates'), 'link' => $flextype->router->urlFor('admin.templates.index'), 'attributes' => ['class' => 'nav-link']]);
+    $flextype->registry->set('admin_navigation.extends.snippets', ['title' => '<i class="far fa-file-code"></i>' . __('admin_snippets'), 'link' => $flextype->router->urlFor('admin.snippets.index'), 'attributes' => ['class' => 'nav-link']]);
+    $flextype->registry->set('admin_navigation.extends.plugins', ['title' => '<i class="fas fa-plug"></i>' . __('admin_plugins'), 'link' => $flextype->router->urlFor('admin.plugins.index'), 'attributes' => ['class' => 'nav-link']]);
+    $flextype->registry->set('admin_navigation.settings.settings', ['title' => '<i class="fas fa-cog"></i>' . __('admin_settings'), 'link' => $flextype->router->urlFor('admin.settings.index'), 'attributes' => ['class' => 'nav-link']]);
+    $flextype->registry->set('admin_navigation.settings.infomation', ['title' => '<i class="fas fa-info"></i>' . __('admin_information'), 'link' => $flextype->router->urlFor('admin.information.index'), 'attributes' => ['class' => 'nav-link']]);
 
     $flextype['SettingsController'] = function($container) {
         return new SettingsController($container);
@@ -58,5 +67,9 @@ if (isset($uri) && isset($uri[0]) && $uri[0] == 'admin') {
 
     $flextype['TemplatesController'] = function($container) {
         return new TemplatesController($container);
+    };
+
+    $flextype['UsersController'] = function($container) {
+        return new UsersController($container);
     };
 }
