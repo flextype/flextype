f6477bb5d238926d82c3cf7cb2237de6a86d54cc
Awilum <awilum@msn.com>
2019-06-02 16:24:58 +0300

    Editor.js integration #122 #117 - trumbowyg removed

--- site/plugins/admin/gulpfile.js
+++ site/plugins/admin/gulpfile.js
@@ -44,8 +44,6 @@ gulp.task('admin-css-clean', function() {
 gulp.task('build-css', function(){
   return gulp.src(['node_modules/bootstrap/dist/css/bootstrap.min.css',
                    'node_modules/animate.css/animate.min.css',
-                   'node_modules/trumbowyg/dist/ui/trumbowyg.min.css',
-                   'node_modules/trumbowyg/dist/plugins/table/ui/trumbowyg.table.css',
                    'node_modules/codemirror/lib/codemirror.css',
                    'node_modules/messenger-hubspot/build/css/messenger.css',
                    'node_modules/messenger-hubspot/build/css/messenger-theme-flat.css',
@@ -64,9 +62,6 @@ gulp.task('js', function(){
   return gulp.src(['node_modules/jquery/dist/jquery.min.js',
                    'node_modules/popper.js/dist/umd/popper.min.js',
                    'node_modules/bootstrap/dist/js/bootstrap.min.js',
-                   'node_modules/trumbowyg/dist/trumbowyg.min.js',
-                   'node_modules/trumbowyg/dist/plugins/noembed/trumbowyg.noembed.js',
-                   'node_modules/trumbowyg/dist/plugins/table/trumbowyg.table.js',
                    'node_modules/codemirror/lib/codemirror.js',
                    'node_modules/codemirror/mode/htmlmixed/htmlmixed.js',
                    'node_modules/codemirror/mode/xml/xml.js',
@@ -85,21 +80,6 @@ gulp.task('js', function(){
     .pipe(gulp.dest('assets/dist/js/'));
 });
 
-gulp.task('trumbowyg-fonts', function(){
-  return gulp.src(['node_modules/trumbowyg/dist/ui/icons.svg'])
-    .pipe(gulp.dest('assets/dist/fonts/trumbowyg'));
-});
-
-gulp.task('trumbowyg-fonts', function(){
-  return gulp.src(['node_modules/trumbowyg/dist/ui/icons.svg'])
-    .pipe(gulp.dest('assets/dist/fonts/trumbowyg'));
-});
-
-gulp.task('trumbowyg-langs', function(){
-  return gulp.src(['node_modules/trumbowyg/dist/*langs/**/*'])
-    .pipe(gulp.dest('assets/dist/langs/trumbowyg'));
-});
-
 gulp.task('codemirror-theme-monokai', function(){
   return gulp.src(['node_modules/codemirror/theme/monokai.css'])
     .pipe(gulp.dest('assets/dist/css/'));
@@ -113,8 +93,6 @@ gulp.task('codemirror-theme-elegant', function(){
 gulp.task('default', function(callback) {
   runSequence('admin-css',
                         ['build-css',
-                        'trumbowyg-fonts',
-                        'trumbowyg-langs',
                         'js'],
               'admin-css-clean',
               'admin-light-css',
