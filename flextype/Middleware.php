2f5fce57622a25b467f1f7c15f4c2b94d1f23688
Awilum <awilum@msn.com>
2019-06-07 13:57:39 +0300

    Flextype Core: update base Middleware class #117

--- flextype/Middleware.php
+++ flextype/Middleware.php
@@ -20,4 +20,11 @@ class Middleware
     {
         $this->container = $container;
     }
+
+    public function __get($property)
+    {
+        if ($this->container->{$property}) {
+            return $this->container->{$property};
+        }
+    }
 }
