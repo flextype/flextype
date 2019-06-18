084bab1288d09e0e043a75a8741b31f48e8f10a4
Awilum <awilum@msn.com>
2019-06-18 16:51:03 +0300

    Flextype Core: GlobalVarsTwigExtension #154 - GlobalVarsTwigExtension `flextype_version` removed. - GlobalVarsTwigExtension `FLEXTYPE_VERSION` added.

--- flextype/twig/GlobalVarsTwigExtension.php
+++ flextype/twig/GlobalVarsTwigExtension.php
@@ -39,7 +39,7 @@ class GlobalVarsTwigExtension extends \Twig_Extension implements \Twig_Extension
             'PATH_CONFIG_DEFAULT' => PATH['config']['default'],
             'PATH_CONFIG_SITE' => PATH['config']['site'],
             'PATH_CACHE' => PATH['cache'],
-            'flextype_version' => FLEXTYPE_VERSION,
+            'FLEXTYPE_VERSION' => FLEXTYPE_VERSION,
             'registry' => $this->flextype['registry']->dump()
         ];
     }
