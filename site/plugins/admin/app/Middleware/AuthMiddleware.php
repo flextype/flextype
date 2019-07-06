ca23097154eea020acbc8b0fdf0e7bdc373d8b96
Awilum <awilum@msn.com>
2019-07-06 19:59:52 +0300

    - Flextype Admin Panel: general code cleanup and refactoring #182

--- site/plugins/admin/app/Middleware/AuthMiddleware.php
+++ site/plugins/admin/app/Middleware/AuthMiddleware.php
@@ -1,12 +1,37 @@
 <?php
 
+/**
+ * @package Flextype
+ *
+ * @author Sergey Romanenko <hello@romanenko.digital>
+ * @link http://romanenko.digital
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Flextype;
 
 use Flextype\Component\Session\Session;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
+/**
+ * @property Router $router
+ */
 class AuthMiddleware extends Middleware
 {
-    public function __invoke($request, $response, $next)
+
+    /**
+     * __invoke
+     *
+     * @param Request  $request  PSR7 request
+     * @param Response $response PSR7 response
+     * @param callable $next     Next middleware
+     *
+     * @return Response
+     */
+    public function __invoke(Request $request, Response $response, $next) : Response
     {
         if (Session::exists('role') && Session::get('role') == 'admin') {
             $response = $next($request, $response);
