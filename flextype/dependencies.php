9b77bcd6ff4d720c94d26276fc65d318f6cfbc97
Awilum <awilum@msn.com>
2019-08-05 20:00:55 +0300

    Flextype Core and Plugins: Doctrine Coding Standard #200 #186 - next round of code improvements

--- flextype/dependencies.php
+++ flextype/dependencies.php
@@ -1,13 +1,10 @@
 <?php
 
 /**
- * @package Flextype
+ * Flextype (http://flextype.org)
+ * Maintained by Sergey Romanenko and Flextype Community.
  *
- * @author Romanenko Sergey <hello@romanenko.digital>
- * @link http://romanenko.digital
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * @license https://github.com/flextype/flextype/blob/master/LICENSE.txt (MIT License)
  */
 
 namespace Flextype;
