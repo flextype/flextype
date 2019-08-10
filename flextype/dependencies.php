17bb84402d8f152299494d92b87625885743320f
Awilum <awilum@msn.com>
2019-08-11 00:39:50 +0300

    Flextype Core and Plugins: Doctrine Coding Standard #200 #186 - next round of code improvements

--- flextype/dependencies.php
+++ flextype/dependencies.php
@@ -1,185 +1,210 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * Flextype (http://flextype.org)
  * Founded by Sergey Romanenko and maintained Flextype Community.
- *
- * @license https://github.com/flextype/flextype/blob/master/LICENSE.txt (MIT License)
  */
 
 namespace Flextype;
 
-use Flextype\Component\Registry\Registry;
-use Thunder\Shortcode\ShortcodeFacade;
-use Slim\Flash\Messages;
 use Cocur\Slugify\Slugify;
-use League\Glide\ServerFactory;
-use League\Glide\Responses\SlimResponseFactory;
+use Intervention\Image\ImageManager;
 use League\Event\Emitter;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Filesystem;
+use League\Glide\Api\Api;
+use League\Glide\Manipulators\Background;
+use League\Glide\Manipulators\Blur;
+use League\Glide\Manipulators\Border;
+use League\Glide\Manipulators\Brightness;
+use League\Glide\Manipulators\Contrast;
+use League\Glide\Manipulators\Crop;
+use League\Glide\Manipulators\Encode;
+use League\Glide\Manipulators\Filter;
+use League\Glide\Manipulators\Gamma;
+use League\Glide\Manipulators\Orientation;
+use League\Glide\Manipulators\Pixelate;
+use League\Glide\Manipulators\Sharpen;
+use League\Glide\Manipulators\Size;
+use League\Glide\Manipulators\Watermark;
+use League\Glide\Responses\SlimResponseFactory;
+use League\Glide\ServerFactory;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Slim\Csrf\Guard;
+use Slim\Flash\Messages;
+use Slim\Http\Environment;
+use Slim\Http\Uri;
+use Slim\Views\Twig;
+use Slim\Views\TwigExtension;
+use Thunder\Shortcode\ShortcodeFacade;
+use Twig\Extension\DebugExtension;
+use function date;
 
 /**
  * Add registry service to Flextype container
  */
-$flextype['registry'] = function ($container) use ($registry) {
+$flextype['registry'] = static function ($container) use ($registry) {
     return $registry;
 };
 
 /**
  * Add CSRF (cross-site request forgery) protection service to Flextype container
  */
-$flextype['csrf'] = function ($container) {
-    return new \Slim\Csrf\Guard;
+$flextype['csrf'] = static function ($container) {
+    return new Guard();
 };
 
 /**
  * Add logger
  */
-$flextype['logger'] = function($container) {
-    $logger = new \Monolog\Logger('flextype');
-    $file_handler = new \Monolog\Handler\StreamHandler(PATH['site'] . '/logs/' . date('Y-m-d') . '.log');
+$flextype['logger'] = static function ($container) {
+    $logger       = new Logger('flextype');
+    $file_handler = new StreamHandler(PATH['site'] . '/logs/' . date('Y-m-d') . '.log');
     $logger->pushHandler($file_handler);
+
     return $logger;
 };
 
 /**
  * Add emitter service to Flextype container
  */
-$flextype['emitter'] = function ($container) {
+$flextype['emitter'] = static function ($container) {
     return new Emitter();
 };
 
 /**
  * Add slugify service to Flextype container
  */
-$flextype['slugify'] = function ($container) {
-    return new Slugify(['separator' => $container['registry']->get('settings.slugify.separator'),
-                        'lowercase' => $container['registry']->get('settings.slugify.lowercase'),
-                        'trim' => $container['registry']->get('settings.slugify.trim'),
-                        'regexp' => $container['registry']->get('settings.slugify.regexp'),
-                        'lowercase_after_regexp' => $container['registry']->get('settings.slugify.lowercase_after_regexp'),
-                        'strip_tags' => $container['registry']->get('settings.slugify.strip_tags'),]);
+$flextype['slugify'] = static function ($container) {
+    return new Slugify([
+        'separator' => $container['registry']->get('settings.slugify.separator'),
+        'lowercase' => $container['registry']->get('settings.slugify.lowercase'),
+        'trim' => $container['registry']->get('settings.slugify.trim'),
+        'regexp' => $container['registry']->get('settings.slugify.regexp'),
+        'lowercase_after_regexp' => $container['registry']->get('settings.slugify.lowercase_after_regexp'),
+        'strip_tags' => $container['registry']->get('settings.slugify.strip_tags'),
+    ]);
 };
 
 /**
  * Add flash service to Flextype container
  */
-$flextype['flash'] = function ($container) {
+$flextype['flash'] = static function ($container) {
     return new Messages();
 };
 
 /**
  * Add cache service to Flextype container
  */
-$flextype['cache'] = function ($container) use ($flextype) {
+$flextype['cache'] = static function ($container) use ($flextype) {
     return new Cache($flextype);
 };
 
 /**
  * Add images service to Flextype container
  */
-$flextype['images'] = function ($container) {
-
+$flextype['images'] = static function ($container) {
     // Get images settings
     $imagesSettings = $container->get('settings')['images'];
 
     // Set source filesystem
-    $source = new \League\Flysystem\Filesystem(
-        new \League\Flysystem\Adapter\Local(PATH['entries'])
+    $source = new Filesystem(
+        new Local(PATH['entries'])
     );
 
     // Set cache filesystem
-    $cache = new \League\Flysystem\Filesystem(
-        new \League\Flysystem\Adapter\Local(PATH['cache'] . '/glide')
+    $cache = new Filesystem(
+        new Local(PATH['cache'] . '/glide')
     );
 
     // Set watermarks filesystem
-    $watermarks = new \League\Flysystem\Filesystem(
-        new \League\Flysystem\Adapter\Local(PATH['site'] . '/watermarks')
+    $watermarks = new Filesystem(
+        new Local(PATH['site'] . '/watermarks')
     );
 
     // Set image manager
-    $imageManager = new \Intervention\Image\ImageManager($imagesSettings);
+    $imageManager = new ImageManager($imagesSettings);
 
     // Set manipulators
     $manipulators = [
-        new \League\Glide\Manipulators\Orientation(),
-        new \League\Glide\Manipulators\Crop(),
-        new \League\Glide\Manipulators\Size(2000*2000),
-        new \League\Glide\Manipulators\Brightness(),
-        new \League\Glide\Manipulators\Contrast(),
-        new \League\Glide\Manipulators\Gamma(),
-        new \League\Glide\Manipulators\Sharpen(),
-        new \League\Glide\Manipulators\Filter(),
-        new \League\Glide\Manipulators\Blur(),
-        new \League\Glide\Manipulators\Pixelate(),
-        new \League\Glide\Manipulators\Watermark($watermarks),
-        new \League\Glide\Manipulators\Background(),
-        new \League\Glide\Manipulators\Border(),
-        new \League\Glide\Manipulators\Encode(),
+        new Orientation(),
+        new Crop(),
+        new Size(2000*2000),
+        new Brightness(),
+        new Contrast(),
+        new Gamma(),
+        new Sharpen(),
+        new Filter(),
+        new Blur(),
+        new Pixelate(),
+        new Watermark($watermarks),
+        new Background(),
+        new Border(),
+        new Encode(),
     ];
 
     // Set API
-    $api = new \League\Glide\Api\Api($imageManager, $manipulators);
+    $api = new Api($imageManager, $manipulators);
 
     // Setup Glide server
-    $server = \League\Glide\ServerFactory::create([
+    return ServerFactory::create([
         'source' => $source,
         'cache' => $cache,
         'api' => $api,
         'response' => new SlimResponseFactory(),
     ]);
-
-    return $server;
 };
 
 /**
  * Add fieldsets service to Flextype container
  */
-$flextype['fieldsets'] = function ($container) use ($flextype) {
+$flextype['fieldsets'] = static function ($container) use ($flextype) {
     return new Fieldsets($flextype);
 };
 
 /**
  * Add snippets service to Flextype container
  */
-$flextype['snippets'] = function ($container) use ($flextype, $app) {
+$flextype['snippets'] = static function ($container) use ($flextype, $app) {
     return new Snippets($flextype, $app);
 };
 
 /**
  * Add shortcodes service to Flextype container
  */
-$flextype['shortcodes'] = function ($container) {
+$flextype['shortcodes'] = static function ($container) {
     return new ShortcodeFacade();
 };
 
 /**
  * Add entries service to Flextype container
  */
-$flextype['entries'] = function ($container) {
+$flextype['entries'] = static function ($container) {
     return new Entries($container);
 };
 
 /**
  * Add view service to Flextype container
  */
-$flextype['view'] = function ($container) {
-
+$flextype['view'] = static function ($container) {
     // Get twig settings
     $twigSettings = $container->get('settings')['twig'];
 
     // Create Twig View
-    $view = new \Slim\Views\Twig(PATH['site'], $twigSettings);
+    $view = new Twig(PATH['site'], $twigSettings);
 
     // Instantiate
     $router = $container->get('router');
-    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $uri    = Uri::createFromEnvironment(new Environment($_SERVER));
 
     // Add Twig Extension
-    $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
+    $view->addExtension(new TwigExtension($router, $uri));
 
     // Add Twig Debug Extension
-    $view->addExtension(new \Twig\Extension\DebugExtension());
+    $view->addExtension(new DebugExtension());
 
     // Add Entries Twig Extension
     $view->addExtension(new EntriesTwigExtension($container));
@@ -221,13 +246,13 @@ $flextype['view'] = function ($container) {
 /**
  * Add themes service to Flextype container
  */
-$flextype['themes'] = function ($container) use ($flextype, $app) {
+$flextype['themes'] = static function ($container) use ($flextype, $app) {
      return new Themes($flextype, $app);
 };
 
 /**
  * Add plugins service to Flextype container
  */
-$flextype['plugins'] = function ($container) use ($flextype, $app) {
+$flextype['plugins'] = static function ($container) use ($flextype, $app) {
      return new Plugins($flextype, $app);
 };
