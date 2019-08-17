a17a88df8f8e4da7caac1f47dcbc04cca7051662
Awilum <awilum@msn.com>
2019-08-17 12:22:15 +0300

    Flextype Core: Add ability to work with different types of content #212 #186 - change parser order

--- flextype/parsers/Parser.php
+++ flextype/parsers/Parser.php
@@ -12,31 +12,32 @@ namespace Flextype;
 class Parser
 {
     public static $drivers = [
+        'frontmatter' => [
+           'name' => 'frontmatter',
+           'ext' => 'md',
+        ],
         'json' => [
             'name' => 'json',
             'ext' => 'json',
         ], 'yaml' => [
             'name' => 'yaml',
             'ext' => 'yaml',
-        ], 'frontmatter' => [
-            'name' => 'frontmatter',
-            'ext' => 'md',
-        ],
+        ]
     ];
 
     public static function encode($input, string $driver) : string
     {
         switch ($driver) {
+            case 'frontmatter':
+                return FrontmatterParser::encode($input);
+
+                break;
             case 'json':
                 return JsonParser::encode($input);
 
                 break;
             case 'yaml':
                 return YamlParser::encode($input);
-
-                break;
-            case 'frontmatter':
-                return FrontmatterParser::encode($input);
 
                 break;
             default:
@@ -48,16 +49,16 @@ class Parser
     public static function decode(string $input, string $driver)
     {
         switch ($driver) {
+            case 'frontmatter':
+                return FrontmatterParser::decode($input);
+
+                break;
             case 'json':
                 return JsonParser::decode($input);
 
                 break;
             case 'yaml':
                 return YamlParser::decode($input);
-
-                break;
-            case 'frontmatter':
-                return FrontmatterParser::decode($input);
 
                 break;
             default:
