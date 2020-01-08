b277287c68fef90edfbe9af45908624cfa7b9df0
Awilum <awilum@msn.com>
2020-01-09 00:15:57 +0300

    refactor(admin-plugin): fix code standard for ThemesController #347

--- site/plugins/admin/app/Controllers/ThemesController.php
+++ site/plugins/admin/app/Controllers/ThemesController.php
@@ -8,9 +8,12 @@ use Flextype\Component\Arr\Arr;
 use Flextype\Component\Filesystem\Filesystem;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use function array_merge;
+use function array_replace_recursive;
 use function count;
 use function Flextype\Component\I18n\__;
 use function is_array;
+use function trim;
 
 /**
  * @property View $view
@@ -91,7 +94,6 @@ class ThemesController extends Controller
                 if (! Filesystem::has($theme_settings_file = $site_theme_settings_dir . $theme['dirname'] . '/settings.yaml')) {
                     continue;
                 }
-
 
                 if (($content = Filesystem::read($theme_settings_file)) === false) {
                     throw new RuntimeException('Load file: ' . $theme_settings_file . ' - failed!');
