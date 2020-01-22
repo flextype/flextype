8191fe73d1b5d203a8f8c48176c4179edc4d9d44
Awilum <awilum@msn.com>
2020-01-22 22:47:39 +0300

    feat(admin-plugin): add custom grayscale #211

--- site/plugins/admin/tailwind.config.js
+++ site/plugins/admin/tailwind.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-  theme: {},
+  theme: {
+      extend: {
+          colors: {
+              gray: {
+                  '000': '#f9f9f9',
+                  '100': '#ededed',
+                  '200': '#e1e1e1',
+                  '300': '#d3d3d3',
+                  '400': '#c4c4c4',
+                  '500': '#b3b3b3',
+                  '600': '#a0a0a0',
+                  '700': '#898989',
+                  '800': '#6c6c6c',
+                  '800': '#3f3f3f',
+              }
+          }
+      }
+  },
   variants: {},
   plugins: [],
 }
