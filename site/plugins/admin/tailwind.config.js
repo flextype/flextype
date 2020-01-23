f1382717f0a8a6e3f6931a8dbbf5edfb253c0166
Awilum <awilum@msn.com>
2020-01-24 00:42:17 +0300

    feat(admin-plugin): update tailwind.config.js #211

--- site/plugins/admin/tailwind.config.js
+++ site/plugins/admin/tailwind.config.js
@@ -1,21 +1,26 @@
 module.exports = {
   theme: {
-      extend: {
-          colors: {
-              gray: {
-                  '000': '#f9f9f9',
-                  '100': '#ededed',
-                  '200': '#e1e1e1',
-                  '300': '#d3d3d3',
-                  '400': '#c4c4c4',
-                  '500': '#b3b3b3',
-                  '600': '#a0a0a0',
-                  '700': '#898989',
-                  '800': '#6c6c6c',
-                  '900': '#3f3f3f',
-              }
+      colors: {
+          black: '#000',
+          white: '#fff',
+          gray: {
+              '000': '#f9f9f9',
+              '100': '#ededed',
+              '200': '#e1e1e1',
+              '300': '#d3d3d3',
+              '400': '#c4c4c4',
+              '500': '#b3b3b3',
+              '600': '#a0a0a0',
+              '700': '#898989',
+              '800': '#6c6c6c',
+              '900': '#3f3f3f',
+              '1000': '#16171a',
           }
-      }
+      },
+      borderColor: theme => ({
+        ...theme('colors'),
+        default: theme('colors.black', 'currentColor'),
+    }),
   },
   variants: {},
   plugins: [],
