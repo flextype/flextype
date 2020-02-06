97586d4bb7707ba47cbdb5dff853d4a626b196c8
Awilum <awilum@msn.com>
2020-02-06 19:42:08 +0300

    feat(form-plugin): add Form plugin #360

---
+++ site/plugins/form/dependencies.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @link http://digital.flextype.org
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flextype;
+
+/**
+ * Add forms controller to Flextype container
+ */
+$flextype['FormController'] = static function ($container) {
+    return new FormController($container);
+};
---
+++ site/plugins/form/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @link http://digital.flextype.org
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flextype;
