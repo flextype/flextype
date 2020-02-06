418ef7073f39cb8ba887c50eecc21e4f45388c0a
Awilum <awilum@msn.com>
2020-02-06 19:18:14 +0300

    feat(admin-plugin): remove Forms API from Flextype core #360

--- flextype/dependencies.php
+++ flextype/dependencies.php
@@ -203,13 +203,6 @@ $flextype['fieldsets'] = static function ($container) use ($flextype) {
 };
 
 /**
- * Add forms service to Flextype container
- */
-$flextype['forms'] = static function ($container) use ($flextype) {
-    return new Forms($flextype);
-};
-
-/**
  * Add snippets service to Flextype container
  */
 $flextype['snippets'] = static function ($container) use ($flextype, $app) {
