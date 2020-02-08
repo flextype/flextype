11adb0a5f49d9de545fd27436853ba146de6c9ee
Awilum <awilum@msn.com>
2020-02-08 15:29:46 +0300

    feat(form-plugin): add select field #360

--- site/plugins/form/app/Controllers/FormController.php
+++ site/plugins/form/app/Controllers/FormController.php
@@ -376,22 +376,16 @@ class FormController extends Controller
      */
     protected function selectField(string $field_id, string $field_name, $field_value, array $properties) : string
     {
-        $title = isset($properties['title']) ? $properties['title'] : '';
-        $size  = isset($properties['size'])  ? $this->sizes[$properties['size']] : $this->sizes['12'];
-        $help  = isset($properties['help'])  ? $properties['help'] : '';
+        $title   = isset($properties['title']) ? $properties['title'] : '';
+        $size    = isset($properties['size'])  ? $this->sizes[$properties['size']] : $this->sizes['12'];
+        $help    = isset($properties['help'])  ? $properties['help'] : '';
         $options = isset($properties['options']) ? $properties['options'] : [];
+        $id      = isset($properties['id'])    ? $properties['id'] : $field_id;
+        $class   = isset($properties['class']) ? $properties['class'] . $this->field_class : $this->field_class;
+        $name    = isset($properties['name'])  ? $properties['name'] : $field_name;
+        $current_value   = isset($properties['value']) ? $properties['value'] : $field_value;
 
-        $attributes = isset($properties['attributes']) ? $properties['attributes'] : [];
-        $attributes['id'] = isset($attributes['id']) ? $attributes['id'] : $field_id;
-        $attributes['class'] = isset($attributes['class']) ? $attributes['class'] : $this->field_class . ' js-select';
-
-        $field  = '<div class="form-group ' . $size . '">';
-        $field .= ($title ? Form::label($field_id, __($title)) : '');
-        $field .= Form::select($field_name, $options, $field_value, $attributes);
-        $field .= ($help ? '<small>' . __($help) . '</small>' : '');
-        $field .= '</div>';
-
-        return $field;
+        return $this->flextype['view']->fetch('plugins/form/templates/fields/select/field.html', ['title' => $title, 'size' => $size, 'name' => $name, 'id' => $id, 'class' => $class, 'help' => $help , 'options' => $options, 'current_value' => $current_value]);
     }
 
     /**
