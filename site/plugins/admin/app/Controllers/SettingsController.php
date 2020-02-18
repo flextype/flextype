1ac6fc46f8743a862e2e0a3fb067f47ff89ae33d
Awilum <awilum@msn.com>
2020-02-18 20:34:45 +0300

    feat(admin-plugin): settings rethinking

--- site/plugins/admin/app/Controllers/SettingsController.php
+++ site/plugins/admin/app/Controllers/SettingsController.php
@@ -33,70 +33,11 @@ class SettingsController extends Controller
      */
     public function index(/** @scrutinizer ignore-unused */ Request $request, Response $response) : Response
     {
-        $entries = [];
-        foreach ($this->entries->fetch('', ['order_by' => ['field' => 'published_at', 'direction' => 'desc']]) as $entry) {
-            $entries[$entry['slug']] = $entry['title'];
-        }
-
-        $themes = [];
-        foreach ($this->registry->get('themes') as $key => $theme) {
-            $themes[$key] = $theme['name'];
-        }
-
-        $available_locales = Filesystem::listContents(PATH['plugins'] . '/admin/lang/');
-        $system_locales    = $this->plugins->getLocales();
-        $locales           = [];
-        foreach ($available_locales as $locale) {
-            if ($locale['type'] !== 'file' || $locale['extension'] !== 'yaml') {
-                continue;
-            }
-
-            $locales[$locale['basename']] = $system_locales[$locale['basename']]['nativeName'];
-        }
-
-        $cache_driver = [
-            'auto' => 'Auto Detect',
-            'file' => 'File',
-            'apcu' => 'APCu',
-            'wincache' => 'WinCache',
-            'memcached' => 'Memcached',
-            'redis' => 'Redis',
-            'sqlite3' => 'SQLite3',
-            'zend' => 'Zend',
-            'array' => 'Array',
-        ];
-
-        $image_driver = [
-            'gd' => 'gd',
-            'imagick' => 'imagick',
-        ];
-
-        $whoops_editor = [
-            'emacs' => 'Emacs',
-            'idea' => 'IDEA',
-            'macvim' => 'MacVim',
-            'phpstorm' => 'PhpStorm (macOS only)',
-            'sublime' => 'Sublime Text',
-            'textmate' => 'Textmate',
-            'xdebug' => 'xDebug',
-            'vscode' => 'VSCode',
-            'atom' => 'Atom',
-            'espresso' => 'Espresso',
-        ];
-
         return $this->view->render(
             $response,
             'plugins/admin/templates/system/settings/index.html',
             [
-                'timezones' => Date::timezones(),
-                'date_formats' => $this->dateFormats(),
-                'date_display_format' => $this->displayDateFormats(),
-                'cache_driver' => $cache_driver,
-                'locales' => $locales,
-                'entries' => $entries,
-                'themes' => $themes,
-                'image_driver' => $image_driver,
-                'whoops_editor' => $whoops_editor,
+                'data' => Filesystem::read(PATH['config']['site'] . '/settings.yaml'),
                 'menu_item' => 'settings',
                 'links' => [
                     'settings' => [
@@ -124,24 +65,9 @@ class SettingsController extends Controller
      */
     public function updateSettingsProcess(Request $request, Response $response) : Response
     {
-        $data = $request->getParsedBody();
+        $post_data = $request->getParsedBody();
 
-        Arr::delete($data, 'csrf_name');
-        Arr::delete($data, 'csrf_value');
-        Arr::delete($data, 'action');
-
-        Arr::set($data, 'errors.display', ($data['errors']['display'] === '1'));
-        Arr::set($data, 'cache.enabled', ($data['cache']['enabled'] === '1'));
-        Arr::set($data, 'slugify.lowercase_after_regexp', ($data['slugify']['lowercase_after_regexp'] === '1'));
-        Arr::set($data, 'slugify.strip_tags', ($data['slugify']['strip_tags'] === '1'));
-        Arr::set($data, 'slugify.trim', ($data['slugify']['trim'] === '1'));
-        Arr::set($data, 'slugify.lowercase', ($data['slugify']['lowercase'] === '1'));
-        Arr::set($data, 'cache.lifetime', (int) $data['cache']['lifetime']);
-        Arr::set($data, 'entries.media.upload_images_quality', (int) $data['entries']['media']['upload_images_quality']);
-        Arr::set($data, 'entries.media.upload_images_width', (int) $data['entries']['media']['upload_images_width']);
-        Arr::set($data, 'entries.media.upload_images_height', (int) $data['entries']['media']['upload_images_height']);
-
-        if (Filesystem::write(PATH['config']['site'] . '/settings.yaml', $this->parser->encode(array_merge($this->registry->get('settings'), $data), 'yaml'))) {
+        if (Filesystem::write(PATH['config']['site'] . '/settings.yaml', $post_data['data'])) {
             $this->flash->addMessage('success', __('admin_message_settings_saved'));
         } else {
             $this->flash->addMessage('error', __('admin_message_settings_was_not_saved'));
