d28f46da921c5dd3cd020f6025371aaae60c7cc7
Awilum <awilum@msn.com>
2020-02-21 21:28:37 +0300

    feat(core): add Delivery Registry API

--- flextype/api/delivery/registry.php
+++ flextype/api/delivery/registry.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 
 namespace Flextype;
 
+use Flextype\Component\Filesystem\Filesystem;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
