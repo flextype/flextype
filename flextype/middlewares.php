66e244dcc1711da3ec4bb1c1dd022a62136fa15c
Awilum <awilum@msn.com>
2020-02-25 10:30:06 +0300

    feat(core): try to use native slim cors for api's

--- flextype/middlewares.php
+++ flextype/middlewares.php
@@ -9,14 +9,7 @@ declare(strict_types=1);
 
 namespace Flextype;
 
-use Tuupola\Middleware\CorsMiddleware;
-
 /**
  * Add middleware CSRF (cross-site request forgery) protection for all routes
  */
 $app->add($flextype->get('csrf'));
-
-/**
- * Add middleware CorsMiddleware for Cross-origin resource sharing.
- */
-$app->add(new CorsMiddleware);
