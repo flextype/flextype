97d1e843ed2d42e1e9707b3b54b9d00bb8b2d9b7
Awilum <awilum@msn.com>
2020-03-03 11:28:03 +0300

    Flextype 0.9.7

--- site/plugins/admin/app/Controllers/EntriesController.php
+++ site/plugins/admin/app/Controllers/EntriesController.php
@@ -504,12 +504,10 @@ class EntriesController extends Controller
 
         if (!$this->entries->has($data['parent_entry'] . '/' . $entry_id_current)) {
             if ($this->entries->rename(
-                $entry_id_current,
+                $data['entry_id_path_current'],
                 $data['parent_entry'] . '/' . $entry_id_current
             )) {
-
-                rename(PATH['uploads'] . '/entries/' . $entry_id_current, PATH['uploads'] . '/entries/' . $data['parent_entry'] . '/' . $entry_id_current);
-
+                rename(PATH['uploads'] . '/entries/' . $data['entry_id_path_current'], PATH['uploads'] . '/entries/' . $data['parent_entry'] . '/' . $entry_id_current);
                 $this->clearEntryCounter($data['parent_entry']);
                 $this->flash->addMessage('success', __('admin_message_entry_moved'));
             } else {
