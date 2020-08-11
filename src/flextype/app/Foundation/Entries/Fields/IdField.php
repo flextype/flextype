47618093c09a1b173bc5aace6efe6424748e7bf5
Awilum <awilum@msn.com>
2020-08-11 20:22:23 +0300

    fix(entries): fix wrong Implementation of Slug Field for Entries #452
    - add new field `id`
    
    BREAKING CHANGES
    
    use field `id` to get entry path instead of `slug` field

---
+++ src/flextype/app/Foundation/Entries/Fields/IdField.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Flextype (https://flextype.org)
+ * Founded by Sergey Romanenko and maintained by Flextype Community.
+ */
+
+if ($flextype->registry->get('flextype.settings.entries.fields.id.enabled')) {
+    $flextype->emitter->addListener('onEntryAfterInitialized', function () use ($flextype) : void {
+        $flextype->entries->entry['id'] = isset($flextype->entries->entry['id']) ? (string) $flextype->entries->entry['id'] : (string) ltrim(rtrim($flextype->entries->entry_path, '/'), '/');
+    });
+}
--- src/flextype/app/Foundation/Entries/Fields/SlugField.php
+++ src/flextype/app/Foundation/Entries/Fields/SlugField.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 
 if ($flextype->registry->get('flextype.settings.entries.fields.slug.enabled')) {
     $flextype->emitter->addListener('onEntryAfterInitialized', function () use ($flextype) : void {
-        $flextype->entries->entry['slug'] = isset($flextype->entries->entry['slug']) ? (string) $flextype->entries->entry['slug'] : (string) ltrim(rtrim($flextype->entries->entry_path, '/'), '/');
+        $parts = explode('/', ltrim(rtrim($flextype->entries->entry_path, '/'), '/'));
+        $flextype->entries->entry['slug'] = isset($flextype->entries->entry['slug']) ? (string) $flextype->entries->entry['slug'] : (string) end($parts);
     });
 }
