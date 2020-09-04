af4e96e21ea3fc185cc5c069fde82220f8dc80d4
Awilum <awilum@msn.com>
2020-09-04 14:19:30 +0300

    feat(core): Hide access for all core properties #467

--- src/flextype/Foundation/Entries/Fields/SlugField.php
+++ src/flextype/Foundation/Entries/Fields/SlugField.php
@@ -11,6 +11,6 @@ if (flextype('registry')->get('flextype.settings.entries.fields.slug.enabled')) 
     flextype('emitter')->addListener('onEntryAfterInitialized', static function () : void {
 
         $parts                                         = explode('/', ltrim(rtrim(flextype('entries')->storage['fetch_single']['id'], '/'), '/'));
-        flextype('entries')->storage['fetch_single']['data']['slug'] = 'ads';//isset(flextype('entries')->storage['fetch_single']['data']['slug']) ? (string) flextype('entries')->storage['fetch_single']['data']['slug']: (string) end($parts);
+        flextype('entries')->storage['fetch_single']['data']['slug'] = isset(flextype('entries')->storage['fetch_single']['data']['slug']) ? (string) flextype('entries')->storage['fetch_single']['data']['slug']: (string) end($parts);
     });
 }
