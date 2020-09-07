5e7084de64d2623585111b4ae6226e9027108e80
Awilum <awilum@msn.com>
2020-09-07 21:12:50 +0300

    refactor(cors): update code according to coding doctrine coding standard

--- src/flextype/Foundation/Cors.php
+++ src/flextype/Foundation/Cors.php
@@ -13,7 +13,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 use function count;
-use function flextype;
+use function use Flextype\Support\flextype;
 use function implode;
 
 class Cors
