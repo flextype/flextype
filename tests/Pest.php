a6b6e808975aebe2a865c1ec5b2dedc55363e40d
Awilum <awilum@msn.com>
2020-10-16 14:21:05 +0300

    feat(core): add tests for Flextype Core Functionality. #477

---
+++ tests/Pest.php
@@ -0,0 +1,12 @@
+<?php
+
+define('FLEXTYPE_MINIMUM_PHP', '7.3.0');
+define('ROOT_DIR', str_replace(DIRECTORY_SEPARATOR, '/', getcwd()));
+define('PATH', [
+    'project'   => ROOT_DIR . '/project',
+    'tmp'   => ROOT_DIR . '/var/tmp',
+]);
+
+! is_file($flextype_autoload = ROOT_DIR . '/vendor/autoload.php') and exit('Please run: <i>composer install</i> for flextype');
+$flextype_loader = require_once $flextype_autoload;
+include ROOT_DIR . '/src/flextype/bootstrap.php';
