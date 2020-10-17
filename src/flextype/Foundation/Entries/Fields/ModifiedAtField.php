2188d16485a9b72bbea5fd215d5c13a5de82b1f5
Awilum <awilum@msn.com>
2020-10-18 00:26:30 +0300

    feat(fields): use Atomastic Filesystem for ModifiedAtField #478

--- src/flextype/Foundation/Entries/Fields/ModifiedAtField.php
+++ src/flextype/Foundation/Entries/Fields/ModifiedAtField.php
@@ -7,12 +7,10 @@ declare(strict_types=1);
  * Founded by Sergey Romanenko and maintained by Flextype Community.
  */
 
-use Flextype\Component\Filesystem\Filesystem;
-
 if (flextype('registry')->get('flextype.settings.entries.fields.modified_at.enabled')) {
     flextype('emitter')->addListener('onEntryAfterInitialized', static function (): void {
         if (flextype('entries')->getStorage('fetch_single.data.modified_at') == null) {
-            flextype('entries')->setStorage('fetch_single.data.modified_at', (int) Filesystem::getTimestamp(flextype('entries')->getFileLocation(flextype('entries')->getStorage('fetch_single.id'))));
+            flextype('entries')->setStorage('fetch_single.data.modified_at', (int) flextype('filesystem')->file(flextype('entries')->getFileLocation(flextype('entries')->getStorage('fetch_single.id')))->lastModified());
         }
     });
 }
