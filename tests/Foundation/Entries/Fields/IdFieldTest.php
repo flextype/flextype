a483ba6d2346a31546781460e7996ba7c36ffc03
Awilum <awilum@msn.com>
2020-10-18 00:02:26 +0300

    feat(tests): add tests for entry IdField #477

---
+++ tests/Foundation/Entries/Fields/IdFieldTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Flextype\Component\Filesystem\Filesystem;
+
+beforeEach(function() {
+    filesystem()->directory(PATH['project'] . '/entries')->create();
+});
+
+afterEach(function (): void {
+    filesystem()->directory(PATH['project'] . '/entries')->delete();
+});
+
+test('test IdFieldTest', function () {
+    flextype('entries')->create('foo', []);
+    $id = flextype('entries')->fetch('foo')['id'];
+    $this->assertEquals('foo', $id);
+
+    flextype('entries')->create('foo/bar', []);
+    $id = flextype('entries')->fetch('foo/bar')['id'];
+    $this->assertEquals('foo/bar', $id);
+});
