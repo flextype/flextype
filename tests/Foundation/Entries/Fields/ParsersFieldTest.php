e1de9d9e6de2cb6b7149854e19740c397645eb7e
Awilum <awilum@msn.com>
2020-10-18 00:25:15 +0300

    feat(tests): add tests for entry ParsersField #477

---
+++ tests/Foundation/Entries/Fields/ParsersFieldTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Flextype\Component\Filesystem\Filesystem;
+
+beforeEach(function() {
+    filesystem()->directory(PATH['project'] . '/entries')->create();
+});
+
+afterEach(function (): void {
+    filesystem()->directory(PATH['project'] . '/entries')->delete();
+});
+
+test('test ParsersFieldTest', function () {
+    flextype('entries')->create('foo', ['content' => '#Foo', 'parsers' => ['markdown' => ['enabled' => true, 'fields' => ['content']]]]);
+    $this->assertEquals('<h1>Foo</h1>', flextype('entries')->fetch('foo')['content']);
+
+    flextype('entries')->create('bar', ['content' => '[registry_get name="Bar" default="Zed"]', 'parsers' => ['shortcode' => ['enabled' => true, 'fields' => ['content']]]]);
+    $this->assertEquals('Zed', flextype('entries')->fetch('bar')['content']);
+});
