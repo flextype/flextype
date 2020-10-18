3bf3c0c3b3cb59ca4519b1207a3558e6c9dc4e2d
Awilum <awilum@msn.com>
2020-10-18 22:22:01 +0300

    feat(tests): add tests for MediaFolders move() method #477

--- tests/Foundation/Media/MediaFoldersTest.php
+++ tests/Foundation/Media/MediaFoldersTest.php
@@ -8,9 +8,15 @@ beforeEach(function() {
 });
 
 afterEach(function (): void {
+    filesystem()->directory(PATH['project'] . '/uploads/.meta')->delete();
     filesystem()->directory(PATH['project'] . '/uploads')->delete();
 });
 
 test('test create() method', function () {
     $this->assertTrue(flextype('media_folders')->create('foo'));
+});
+
+test('test move() method', function () {
+    $this->assertTrue(flextype('media_folders')->create('foo'));
+    $this->assertTrue(flextype('media_folders')->move('foo', 'bar'));
 });
