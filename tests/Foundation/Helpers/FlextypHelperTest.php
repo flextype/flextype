a5d0481a35731c0dc724c1ef1e12f2b6b458b7d4
Awilum <awilum@msn.com>
2020-10-18 17:34:37 +0300

    feat(tests): add tests for FlextypeHelper method #477

---
+++ tests/Foundation/Helpers/FlextypHelperTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Flextype\Foundation\Flextype;
+use Flextype\Foundation\Entries\Entries;
+use Atomastic\Strings\Strings;
+
+beforeEach(function() {
+    filesystem()->directory(PATH['project'] . '/entries')->create();
+});
+
+afterEach(function (): void {
+    filesystem()->directory(PATH['project'] . '/entries')->delete();
+});
+
+test('test getVersion() method', function () {
+    $this->assertTrue(!Strings::create(flextype()->getVersion())->isEmpty());
+});
+
+test('test getInstance() method', function () {
+    $firstCall = flextype();
+    $secondCall = Flextype::getInstance();
+
+    $this->assertInstanceOf(Flextype::class, $firstCall);
+    $this->assertSame($firstCall, $secondCall);
+});
+
+test('test container() method', function () {
+    // get container
+    $this->assertInstanceOf(Entries::class, flextype('entries'));
+
+    // set container
+    flextype()->container()['foo'] = 'bar';
+    $this->assertEquals('bar', flextype('foo'));
+});
