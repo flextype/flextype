f64a749eb9c9c92b347a747a94ed5c62388ff824
Awilum <awilum@msn.com>
2020-10-18 14:31:23 +0300

    feat(fields): fix RoutableField logic

--- src/flextype/Foundation/Entries/Fields/RoutableField.php
+++ src/flextype/Foundation/Entries/Fields/RoutableField.php
@@ -10,18 +10,18 @@ declare(strict_types=1);
 
 if (flextype('registry')->get('flextype.settings.entries.fields.routable.enabled')) {
     flextype('emitter')->addListener('onEntryAfterInitialized', static function (): void {
-        if (flextype('entries')->getStorage('fetch_single.data.routable') == null) {
+        if (flextype('entries')->getStorage('fetch_single.data.routable') === null) {
             flextype('entries')->setStorage('fetch_single.data.routable', true);
         } else {
-            flextype('entries')->setStorage('fetch_single.data.routable', (bool) flextype('entries')->getStorage('create.data.routable'));
+            flextype('entries')->setStorage('fetch_single.data.routable', (bool) flextype('entries')->getStorage('fetch_single.data.routable'));
         }
     });
 
     flextype('emitter')->addListener('onEntryCreate', static function (): void {
-        if (flextype('entries')->getStorage('create.data.routable') != null && is_bool(flextype('entries')->getStorage('create.data.routable'))) {
-            flextype('entries')->setStorage('create.data.routable', (bool) flextype('entries')->getStorage('create.data.routable'));
-        } else {
+        if (flextype('entries')->getStorage('create.data.routable') === null) {
             flextype('entries')->setStorage('create.data.routable', true);
+        } else {
+            flextype('entries')->setStorage('create.data.routable', (bool) flextype('entries')->getStorage('create.data.routable'));
         }
     });
 }
