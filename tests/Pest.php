4d7ef735517e32f19c57da89e3f194c3aea9282d
Awilum <awilum@msn.com>
2020-10-18 16:47:05 +0300

    feat(tests): update tests #477

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
+namespace Flextype;
+
 define('FLEXTYPE_MINIMUM_PHP', '7.3.0');
 define('ROOT_DIR', str_replace(DIRECTORY_SEPARATOR, '/', getcwd()));
 define('PATH', [
