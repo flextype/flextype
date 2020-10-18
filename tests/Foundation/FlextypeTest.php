d2312ce92ab974a9ef2303f392026639a5fcce63
Awilum <awilum@msn.com>
2020-10-18 16:46:43 +0300

    feat(tests): add tests for Flextype #477

---
+++ tests/Foundation/FlextypeTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Flextype\Foundation\Flextype;
+use Atomastic\Strings\Strings;
+
+beforeEach(function() {
+    filesystem()->directory(PATH['project'] . '/entries')->create();
+});
+
+afterEach(function (): void {
+    filesystem()->directory(PATH['project'] . '/entries')->delete();
+});
+
+test('test getVersion() method', function () {
+    $this->assertTrue(!Strings::create(flextype()->getVersion())->isEmpty());
+});
