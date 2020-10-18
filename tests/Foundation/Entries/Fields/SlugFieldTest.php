b82bc6314e3f21965cb52cf41032573be46cad78
Awilum <awilum@msn.com>
2020-10-18 14:34:44 +0300

    feat(tests): add tests for entry SlugField #477

---
+++ tests/Foundation/Entries/Fields/SlugFieldTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Flextype\Component\Filesystem\Filesystem;
+
+beforeEach(function() {
+    filesystem()->directory(PATH['project'] . '/entries')->create();
+});
+
+afterEach(function (): void {
+    filesystem()->directory(PATH['project'] . '/entries')->delete();
+});
+
+test('test PublishedByField', function () {
+    flextype('entries')->create('foo', []);
+    $slug = flextype('entries')->fetch('foo')['slug'];
+    $this->assertEquals('foo', $slug);
+
+    flextype('entries')->create('bar', []);
+    $slug = flextype('entries')->fetch('bar')['slug'];
+    $this->assertEquals('bar', $slug);
+});
