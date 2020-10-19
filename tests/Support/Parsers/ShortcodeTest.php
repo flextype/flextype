b89b9a0807715f167b88464e02f36722109aa936
Awilum <awilum@msn.com>
2020-10-20 00:28:57 +0300

    feat(tests): add tests for Shortcode process() method #477

--- tests/Support/Parsers/ShortcodeTest.php
+++ tests/Support/Parsers/ShortcodeTest.php
@@ -17,3 +17,9 @@ test('test parse() method', function () {
     $this->assertTrue(is_array(flextype('shortcode')->parse('[bar]')));
     $this->assertTrue(is_object(flextype('shortcode')->parse('[bar]')[0]));
 });
+
+test('test process() method', function () {
+    $this->assertInstanceOf(ShortcodeFacade::class, flextype('shortcode')->addHandler('zed', static function() { return 'Zed'; }));
+    $this->assertEquals('Zed', flextype('shortcode')->process('[zed]'));
+    $this->assertEquals('fòôBàřZed', flextype('shortcode')->process('fòôBàř[zed]'));
+});
