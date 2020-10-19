175b1f04ff808754649fc5f69eb58bf2d6904b63
Awilum <awilum@msn.com>
2020-10-20 00:20:05 +0300

    feat(tests): add tests for Shortcode addEventHandler() method #477

--- tests/Support/Parsers/ShortcodeTest.php
+++ tests/Support/Parsers/ShortcodeTest.php
@@ -7,3 +7,7 @@ use Thunder\Shortcode\ShortcodeFacade;
 test('test getInstance() method', function () {
     $this->assertInstanceOf(ShortcodeFacade::class, flextype('shortcode')->getInstance());
 });
+
+test('test addEventHandler() method', function () {
+    $this->assertInstanceOf(ShortcodeFacade::class, flextype('shortcode')->addHandler('foo', static function() { return ''; }));
+});
