6779c055de2512bb37e52d74db1f770b853d43eb
Awilum <awilum@msn.com>
2020-10-20 00:25:27 +0300

    feat(tests): add tests for Shortcode parse() method #477

--- tests/Support/Parsers/ShortcodeTest.php
+++ tests/Support/Parsers/ShortcodeTest.php
@@ -11,3 +11,9 @@ test('test getInstance() method', function () {
 test('test addEventHandler() method', function () {
     $this->assertInstanceOf(ShortcodeFacade::class, flextype('shortcode')->addHandler('foo', static function() { return ''; }));
 });
+
+test('test parse() method', function () {
+    $this->assertInstanceOf(ShortcodeFacade::class, flextype('shortcode')->addHandler('bar', static function() { return ''; }));
+    $this->assertTrue(is_array(flextype('shortcode')->parse('[bar]')));
+    $this->assertTrue(is_object(flextype('shortcode')->parse('[bar]')[0]));
+});
