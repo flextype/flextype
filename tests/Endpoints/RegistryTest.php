e82f8940eb83817c21aae96a2056a680a6cb2402
Awilum <awilum@msn.com>
2020-10-30 18:23:12 +0300

    feat(tests): add tests for Registry Endpoints methods #477

---
+++ tests/Endpoints/RegistryTest.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+test('test /api/registry', function () {
+    //$request = flextype()->createJsonRequest('GET', '/api/registry');
+    //$response = flextype()->handle($request);
+    //var_dump($response);
+});
