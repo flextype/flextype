007013b090421db81b631130cca13d4983e0a017
Awilum <awilum@msn.com>
2020-11-02 21:48:11 +0300

    feat(plugins): use Atomastic Components #478

--- src/flextype/Foundation/Plugins.php
+++ src/flextype/Foundation/Plugins.php
@@ -351,7 +351,8 @@ class Plugins
         // Get Plugins List
         $plugins_list = [];
         foreach (flextype('filesystem')->find()->in(PATH['project'] . '/plugins/')->directories()->depth(0) as $plugin) {
-            $plugins_list[]['dirname'] = $plugin->getBasename();
+            $plugins_list[$plugin->getBasename()]['dirname'] = $plugin->getBasename();
+            $plugins_list[$plugin->getBasename()]['pathname'] = $plugin->getPathname();
         }
 
         return $plugins_list;
