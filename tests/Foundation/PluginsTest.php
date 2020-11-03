874a025a6f3b387ca04283b7ce12ff2e3da0e3f8
Awilum <awilum@msn.com>
2020-11-03 13:17:41 +0300

    feat(tests): add tests for Plugins getLocales() and getPluginsDictionary() method #477

--- tests/Foundation/PluginsTest.php
+++ tests/Foundation/PluginsTest.php
@@ -6,12 +6,10 @@ use Flextype\Foundation\Flextype;
 use Atomastic\Strings\Strings;
 
 beforeEach(function() {
-    //filesystem()->directory(PATH['project'] . '/entries')->create();
-
     // Create sandbox plugin
     filesystem()->directory(PATH['project'] . '/plugins/sandbox')->create(0755, true);
     filesystem()->directory(PATH['project'] . '/plugins/sandbox/lang/')->create(0755, true);
-    filesystem()->file(PATH['project'] . '/plugins/sandbox/lang/en_US.yaml')->put('title: Sandbox');
+    filesystem()->file(PATH['project'] . '/plugins/sandbox/lang/en_US.yaml')->put('sandbox_title: Sandbox');
     filesystem()->file(PATH['project'] . '/plugins/sandbox/settings.yaml')->put('enabled: true');
     filesystem()->file(PATH['project'] . '/plugins/sandbox/plugin.yaml')->put('name: Sandbox');
 });
@@ -23,4 +21,14 @@ afterEach(function (): void {
 test('test getPluginsList() method', function () {
     $this->assertTrue(is_array(flextype('plugins')->getPLuginsList()));
     $this->assertTrue(isset(flextype('plugins')->getPLuginsList()['sandbox']));
+});
+
+test('test getLocales() method', function () {
+    $this->assertTrue(is_array(flextype('plugins')->getLocales()));
+    $this->assertTrue(isset(flextype('plugins')->getLocales()['en_US']));
+});
+
+test('test getPluginsDictionary() method', function () {
+    $this->assertTrue(is_array(flextype('plugins')->getPluginsDictionary(flextype('plugins')->getPLuginsList(), 'en_US')));
+    $this->assertTrue(isset(flextype('plugins')->getPluginsDictionary(flextype('plugins')->getPLuginsList(), 'en_US')['en_US']['sandbox_title']));
 });
