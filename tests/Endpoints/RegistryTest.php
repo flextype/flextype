04ed794fa56117bb78982d2cb1aca70e7ee6e68b
Awilum <awilum@msn.com>
2020-11-22 18:10:49 +0300

    feat(tests): update tests for Registry Endpoints #477

--- tests/Endpoints/RegistryTest.php
+++ tests/Endpoints/RegistryTest.php
@@ -10,9 +10,12 @@ beforeEach(function() {
 
 test('test /api/registry', function () {
     $response = $this->client->request('GET', 'https://test.flextype.org/api/registry', [
-        'id' => 'flextype.manifest.version',
-        'token' => 'e15dbc9336c924e31a5b3b4e66f96351'
+        'query' => [
+            'id' => 'flextype.manifest.version',
+            'token' => 'e15dbc9336c924e31a5b3b4e66f96351'
+        ]
     ]);
 
-    $this->assertEquals('0.9.12', json_decode($response->getBody())['data']['key']);
+    $this->assertEquals('flextype.manifest.version', json_decode((string) $response->getBody(), true)['data']['key']);
+    $this->assertEquals('0.9.11', json_decode((string) $response->getBody(), true)['data']['value']);
 });
