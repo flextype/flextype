3fe5c7f0e908c7a32fc0e32726a3ecaa840bdba5
Awilum <awilum@msn.com>
2020-11-22 17:44:56 +0300

    feat(tests): update tests for endpoints #477

--- tests/Endpoints/RegistryTest.php
+++ tests/Endpoints/RegistryTest.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 uses()->group('endpoints');
 
+beforeEach(function() {
+    $this->client = new GuzzleHttp\Client();
+});
+
 test('test /api/registry', function () {
-    $this->assertTrue(true);
+    $response = $this->client->request('GET', 'https://test.flextype.org/api/registry', [
+        'id' => 'flextype.manifest.version',
+        'token' => 'e15dbc9336c924e31a5b3b4e66f96351'
+    ]);
+
+    $this->assertEquals('0.9.12', json_decode($response->getBody())['data']['key']);
 });
