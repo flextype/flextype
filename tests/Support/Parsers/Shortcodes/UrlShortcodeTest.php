60af65bfb8748422616b630dd999741825e7b448
Awilum <awilum@msn.com>
2020-11-23 10:01:34 +0300

    feat(tests): improve tests for UrlShortcode #477

--- tests/Support/Parsers/Shortcodes/UrlShortcodeTest.php
+++ tests/Support/Parsers/Shortcodes/UrlShortcodeTest.php
@@ -4,4 +4,7 @@ declare(strict_types=1);
 
 test('test registry_get shortcode', function () {
     $this->assertStringContainsString('http', flextype('shortcode')->process('[url]'));
+
+    flextype('registry')->set('flextype.settings.url', 'https://flextype.org');
+    $this->assertStringContainsString('https://flextype.org', flextype('shortcode')->process('[url]'));
 });
