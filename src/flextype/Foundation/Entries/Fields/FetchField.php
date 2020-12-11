e596b2ffa4a5586aefd64e063035ea37f0e9066e
Awilum <awilum@msn.com>
2020-12-11 16:47:30 +0300

    feat(fields): Add new field `fetch` for Entries API #492

--- src/flextype/Foundation/Entries/Fields/FetchField.php
+++ src/flextype/Foundation/Entries/Fields/FetchField.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  * Flextype (https://flextype.org)
  * Founded by Sergey Romanenko and maintained by Flextype Community.
  */
-
+ 
 if (flextype('registry')->get('flextype.settings.entries.fields.fetch.enabled')) {
     flextype('emitter')->addListener('onEntriesFetchSingleHasResult', static function (): void {
         if (flextype('entries')->getStorage('fetch.data.fetch') !== null) {
