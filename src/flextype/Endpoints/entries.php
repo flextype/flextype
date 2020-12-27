087a4c5330c1bd682fd1692e7185f1c3de128d51
Awilum <awilum@msn.com>
2020-12-27 21:58:39 +0300

    refactor(entries-rest-api): set strict array type for $request->getParsedBody() #527

--- src/flextype/Endpoints/entries.php
+++ src/flextype/Endpoints/entries.php
@@ -143,7 +143,7 @@ flextype()->get('/api/entries', function (Request $request, Response $response) 
  */
 flextype()->post('/api/entries', function (Request $request, Response $response) use ($api_errors) {
     // Get Post Data
-    $post_data = $request->getParsedBody();
+    $post_data = (array) $request->getParsedBody();
 
     if (! isset($post_data['token']) || ! isset($post_data['access_token']) || ! isset($post_data['id']) || ! isset($post_data['data'])) {
         return $response
@@ -253,7 +253,7 @@ flextype()->post('/api/entries', function (Request $request, Response $response)
  */
 flextype()->patch('/api/entries', function (Request $request, Response $response) use ($api_errors) {
     // Get Post Data
-    $post_data = $request->getParsedBody();
+    $post_data = (array) $request->getParsedBody();
 
     if (! isset($post_data['token']) || ! isset($post_data['access_token']) || ! isset($post_data['id']) || ! isset($post_data['data'])) {
         return $response
@@ -363,7 +363,7 @@ flextype()->patch('/api/entries', function (Request $request, Response $response
  */
 flextype()->put('/api/entries', function (Request $request, Response $response) use ($api_errors) {
     // Get Post Data
-    $post_data = $request->getParsedBody();
+    $post_data = (array) $request->getParsedBody();
 
     if (! isset($post_data['token']) || ! isset($post_data['access_token']) || ! isset($post_data['id']) || ! isset($post_data['new_id'])) {
         return $response
@@ -474,7 +474,7 @@ flextype()->put('/api/entries', function (Request $request, Response $response) 
  */
 flextype()->put('/api/entries/copy', function (Request $request, Response $response) use ($api_errors) {
     // Get Post Data
-    $post_data = $request->getParsedBody();
+    $post_data = (array) $request->getParsedBody();
 
     if (! isset($post_data['token']) || ! isset($post_data['access_token']) || ! isset($post_data['id']) || ! isset($post_data['new_id'])) {
         return $response
@@ -584,7 +584,7 @@ flextype()->put('/api/entries/copy', function (Request $request, Response $respo
  */
 flextype()->delete('/api/entries', function (Request $request, Response $response) use ($api_errors) {
     // Get Post Data
-    $post_data = $request->getParsedBody();
+    $post_data = (array) $request->getParsedBody();
 
     if (! isset($post_data['token']) || ! isset($post_data['access_token']) || ! isset($post_data['id'])) {
         return $response
