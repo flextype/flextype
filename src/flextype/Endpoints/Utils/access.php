c1af3dcd4e24012bf264b1f01638dc9d76a5b9e9
Awilum <sergey.romanenko@flextype.org>
2021-07-17 13:50:41 +0300

    feat(endpoints): rename method `validate_access_token` to `validateAccess_Token`

--- src/flextype/Endpoints/Utils/access.php
+++ src/flextype/Endpoints/Utils/access.php
@@ -14,7 +14,7 @@ use function filesystem;
 /**
  * Validate access token
  */
-function validate_access_token(string $token): bool
+function validateAccessToken(string $token): bool
 {
     return filesystem()->file(PATH['project'] . '/tokens/access/' . $token . '/token.yaml')->exists();
 }
