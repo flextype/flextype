8a584a876f2663db4c94f9a83d0c819361b73693
Awilum <sergey.romanenko@flextype.org>
2021-07-26 22:03:27 +0300

    feat(shortocodes): use entries instead of content

--- src/flextype/Support/Parsers/Shortcodes/EntriesShortcode.php
+++ src/flextype/Support/Parsers/Shortcodes/EntriesShortcode.php
@@ -11,9 +11,9 @@ namespace Flextype\Support\Parsers\Shortcodes;
 
 use Thunder\Shortcode\Shortcode\ShortcodeInterface;
 
-// Shortcode: [content_fetch id="entry-id" field="field-name" default="default-value"]
+// Shortcode: [entries_fetch id="entry-id" field="field-name" default="default-value"]
 if (flextype('registry')->get('flextype.settings.parsers.shortcode.shortcodes.content.enabled')) {
-    flextype('parsers')->shortcode()->addHandler('content_fetch', static function (ShortcodeInterface $s) {
-        return arrays(flextype('content')->fetch($s->getParameter('id')))->get($s->getParameter('field'), $s->getParameter('default'));
+    flextype('parsers')->shortcode()->addHandler('entries_fetch', static function (ShortcodeInterface $s) {
+        return arrays(flextype('entries')->fetch($s->getParameter('id')))->get($s->getParameter('field'), $s->getParameter('default'));
     });
 }
