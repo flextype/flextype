c9c4a9658b777d87cec0043041d9c9e4974ddcff
Awilum <sergey.romanenko@flextype.org>
2021-08-01 10:45:20 +0300

    feat(tests): fixing tests - next round #199

--- tests/Support/Parsers/Shortcodes/UrlShortcodeTest.php
+++ tests/Support/Parsers/Shortcodes/UrlShortcodeTest.php
@@ -3,8 +3,7 @@
 declare(strict_types=1);
 
 test('test registry_get shortcode', function () {
-    $this->assertStringContainsString('http', parsers()->shortcodes()->process('[url]'));
-
     registry()->set('flextype.settings.url', 'https://flextype.org');
+
     $this->assertStringContainsString('https://flextype.org', parsers()->shortcodes()->process('[url]'));
 });
