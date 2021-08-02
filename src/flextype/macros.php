0a0cd6046c71e6df1ed3f36424bac63c46fc67bb
Awilum <sergey.romanenko@flextype.org>
2021-08-02 11:55:04 +0300

    feat(macros): Add ability to fetch specific fields from single entry and from entries collection #558

---
+++ src/flextype/macros.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Flextype (https://flextype.org)
+ * Founded by Sergey Romanenko and maintained by Flextype Community.
+ */
+
+use Atomastic\Arrays\Arrays;
+use Atomastic\Macroable\Macroable;
+
+if (! Arrays::hasMacro('onlyFromCollection')) {
+    /**
+     * Return slice of an array with just a given keys.
+     *
+     * @param array $keys List of keys to return.
+     *
+     * @return Arrays Returns instance of The Arrays class.
+     */
+    Arrays::macro('onlyFromCollection', function(array $keys) {
+        $result = [];
+
+        foreach ($this->toArray() as $key => $value) {
+            $result[$key] = arrays($value)->only($keys)->toArray();
+        }
+
+        return arrays($result);
+    });
+}
+
+if (! Arrays::hasMacro('onlyFromCollection')) {
+    /**
+     * Return slice of an array except given keys.
+     *
+     * @param array $keys List of keys to except.
+     *
+     * @return Arrays Returns instance of The Arrays class.
+     */
+    Arrays::macro('exceptFromCollection', function(array $keys) {
+        $result = [];
+
+        foreach ($this->toArray() as $key => $value) {
+            $result[$key] = arrays($value)->except($keys)->toArray();
+        }
+
+        return arrays($result);
+    });
+}
