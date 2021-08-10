587a2c83ec9d5eed683767768489b3e4a0a26c5d
Awilum <sergey.romanenko@flextype.org>
2021-08-10 12:02:38 +0300

    feat(endpoints): add basic Endpoints class #565

---
+++ src/flextype/core/Endpoints/Endpoints.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Flextype (https://flextype.org)
+ * Founded by Sergey Romanenko and maintained by Flextype Community.
+ */
+
+namespace Flextype\Endpoints;
+
+use Psr\Http\Message\ResponseInterface;
+
+class Endpoints 
+{
+    private array $statusCodeMessages = [
+        '400' => [
+            'title' => 'Bad Request',
+            'message' => 'Validation for this particular item failed',
+        ],
+        '401' => [
+            'title' => 'Unauthorized',
+            'message' => 'Token is wrong',
+        ],
+        '404' => [
+            'title' => 'Not Found',
+            'message' => 'Not Found',
+        ]
+    ];
+
+    public function getApiResponse(ResponseInterface $response, array $body = [], int $status = 200): ResponseInterface
+    {
+        if (count($body) > 0) {
+            $response->getBody()->write(serializers()->json()->encode($body));
+        } 
+
+        $response->withStatus($status);
+        $response->withHeader('Content-Type', 'application/json;charset=' . registry()->get('flextype.settings.charset'));
+        return $response;
+    }
+}
