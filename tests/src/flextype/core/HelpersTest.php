493a906fcc6b1a7ad7c86510e1e8058cfa95a815
Awilum <sergey.romanenko@flextype.org>
2021-08-14 19:55:34 +0300

    feat(tests): update and improve tests

--- tests/src/flextype/core/HelpersTest.php
+++ tests/src/flextype/core/HelpersTest.php
@@ -6,17 +6,22 @@ use Flextype\Flextype;
 use Flextype\Content\Content;
 use Atomastic\Strings\Strings;
 use Slim\App;
+use DI\Container;
 
 test('test flextype() helper', function () {
     $this->assertSame(flextype(), Flextype::getInstance());
+    expect(flextype())->toBeInstanceOf(Flextype::class);
 });
 
 test('test app() helper', function () {
     $this->assertSame(app(), Flextype::getInstance()->app());
+    expect(app())->toBeInstanceOf(App::class);
 });
 
 test('test container() helper', function () {
     $this->assertSame(container(), Flextype::getInstance()->container());
+    $this->assertSame(container(), Flextype::getInstance()->app()->getContainer());
+    expect(container())->toBeInstanceOf(Container::class);
 });
 
 test('test emitter() helper', function () {
