a61fb13d6e4430c0df96dc3086d5893fbbc1f551
Awilum <sergey.romanenko@flextype.org>
2021-08-17 20:18:26 +0300

    feat(shortcodes): rename Content to Entries shortcode #563

---
+++ src/flextype/core/Parsers/Shortcodes/EntriesShortcode.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Flextype (https://flextype.org)
+ * Founded by Sergey Romanenko and maintained by Flextype Community.
+ */
+
+namespace Flextype\Parsers\Shortcodes;
+
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
+
+use function arrays;
+use function entries;
+use function parsers;
+use function registry;
+
+// Shortcode: [entries_fetch id="entry-id" field="field-name" default="default-value"]
+parsers()->shortcodes()->addHandler('entries_fetch', static function (ShortcodeInterface $s) {
+    if (! registry()->get('flextype.settings.parsers.shortcodes.shortcodes.entries.enabled')) {
+        return '';
+    }
+
+    return arrays(entries()->fetch($s->getParameter('id')))->get($s->getParameter('field'), $s->getParameter('default'));
+});
