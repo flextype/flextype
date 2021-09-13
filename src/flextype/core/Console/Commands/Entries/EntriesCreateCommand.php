ecdb1ad672f7bcfa3fedda6aa991aba7c88b734c
Awilum <sergey.romanenko@flextype.org>
2021-09-13 23:43:19 +0300

    feat(console): update EntriesCreateCommand #543

---
+++ src/flextype/core/Console/Commands/Entries/EntriesCreateCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Flextype (https://flextype.org)
+ * Founded by Sergey Romanenko and maintained by Flextype Community.
+ */
+
+namespace Flextype\Console\Commands\Entries;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class EntriesCreateCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this->setName('entries:create');
+        $this->setDescription('Create entry.');
+        $this->addOption('id', null, InputOption::VALUE_REQUIRED, 'Unique identifier of the entry.');
+        $this->addOption('data', null, InputOption::VALUE_OPTIONAL, 'Data to create for the entry.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $data = serializers()->json()->decode($input->getOption('data') ?? []);
+
+        if (entries()->create($input->getOption('id'), $data)) {
+            $io->success('Created entry ' . $input->getOption('id'));
+            return Command::SUCCESS;
+        } else {
+            $io->error('Entry ' . $input->getOption('id') . ' wasn\'t created.');
+            return Command::FAILURE;
+        }
+    }
+}
