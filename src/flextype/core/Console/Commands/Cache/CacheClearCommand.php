fff0e6ebf38f9041d1e6f28e42a2978550d8c86d
Awilum <sergey.romanenko@flextype.org>
2021-09-21 13:44:35 +0300

    feat(console): add CacheClearCommand #543

---
+++ src/flextype/core/Console/Commands/Cache/CacheClearCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Flextype (https://flextype.org)
+ * Founded by Sergey Romanenko and maintained by Flextype Community.
+ */
+
+namespace Flextype\Console\Commands\Cache;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class CacheClearCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this->setName('cache:clear');
+        $this->setDescription('Completely empty the cache.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        if (cache()->clear()) {
+            $io->success('Cache cleared.');
+            return Command::SUCCESS;
+        } else {
+            $io->error('Key wasn\'t cleared.');
+            return Command::FAILURE;
+        }
+    }
+}
