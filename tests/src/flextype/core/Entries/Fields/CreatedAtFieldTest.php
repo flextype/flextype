1eec78d738e064d728e155e9264c5553204f72bd
Sergey Romanenko <sergey.romanenko@flextype.org>
2022-01-29 12:16:45 +0300

    fix(tests): remove ctype_digit check

--- tests/src/flextype/core/Entries/Fields/CreatedAtFieldTest.php
+++ tests/src/flextype/core/Entries/Fields/CreatedAtFieldTest.php
@@ -15,5 +15,5 @@ test('CreatedAtField', function () {
     entries()->create('foo', []);
     $created_at = entries()->fetch('foo')['created_at'];
     $this->assertTrue(strlen($created_at) > 0);
-    $this->assertTrue((ctype_digit($created_at) && strtotime(date('Y-m-d H:i:s', $created_at)) === (int)$created_at));
+    $this->assertTrue((strtotime(date('Y-m-d H:i:s', $created_at)) === (int)$created_at));
 });
