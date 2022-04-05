6f73d54280a582bd0d2d50267baa92ec5284cd56
Awilum <sergey.romanenko@flextype.org>
2022-04-05 16:46:55 +0300

    chore(flextype): update copyright information

--- src/flextype/core/Entries/Entries.php
+++ src/flextype/core/Entries/Entries.php
@@ -2,9 +2,16 @@
 
 declare(strict_types=1);
 
-/**
- * Flextype (https://awilum.github.io/flextype)
- * Founded by Sergey Romanenko and maintained by Flextype Community.
+ /**
+ * Flextype - Hybrid Content Management System with the freedom of a headless CMS 
+ * and with the full functionality of a traditional CMS!
+ * 
+ * Copyright (c) Sergey Romanenko (https://awilum.github.io)
+ *
+ * Licensed under The MIT License.
+ *
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
  */
 
 namespace Flextype\Entries;
