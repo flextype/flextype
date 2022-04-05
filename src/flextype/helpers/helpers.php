dc8fc4916009a94a341fbe530c87c1cd4d895b3b
Awilum <sergey.romanenko@flextype.org>
2022-04-05 17:06:07 +0300

    refactor(flextype): use collection instead of arrays for arrays collecti…

--- src/flextype/helpers/helpers.php
+++ src/flextype/helpers/helpers.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/i18n.php';
 require_once __DIR__ . '/services.php';
-require_once __DIR__ . '/arrays.php';
+require_once __DIR__ . '/collection.php';
 require_once __DIR__ . '/finder.php';
 require_once __DIR__ . '/image.php';
 require_once __DIR__ . '/upload.php';
