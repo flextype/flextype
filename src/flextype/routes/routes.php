7ca7efdbf434907cdabe0ee0ea04db9ddfaf0fe2
Awilum <sergey.romanenko@flextype.org>
2022-04-06 21:59:55 +0300

    feat(routes): add ability to add project routes

--- src/flextype/routes/routes.php
+++ src/flextype/routes/routes.php
@@ -2,6 +2,12 @@
 
 declare(strict_types=1);
 
+// Add endpoints routes
 require_once __DIR__ . '/endpoints/utils.php';
 require_once __DIR__ . '/endpoints/entries.php';
 require_once __DIR__ . '/endpoints/registry.php';
+
+// Add project routes
+if (filesystem()->file(PATH['project'] . '/routes/routes.php')->exists()) {
+    require_once PATH['project'] . '/routes/routes.php';
+}
