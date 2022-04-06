5a642eef3902a93f4e813bcb2424de1ad413b864
Awilum <sergey.romanenko@flextype.org>
2022-04-06 11:43:47 +0300

    feat(routes): add common routes entry point

---
+++ src/flextype/routes/routes.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/endpoints/utils.php';
+require_once __DIR__ . '/endpoints/entries.php';
+require_once __DIR__ . '/endpoints/registry.php';
