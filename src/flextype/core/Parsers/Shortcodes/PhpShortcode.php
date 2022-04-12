b5d3678f4cf5035a5a7d653473f9580b45357639
Awilum <sergey.romanenko@flextype.org>
2022-04-12 12:04:43 +0300

    feat(shortcodes): add [php] shortcode

---
+++ src/flextype/core/Parsers/Shortcodes/PhpShortcode.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+ /**
+ * Flextype - Hybrid Content Management System with the freedom of a headless CMS 
+ * and with the full functionality of a traditional CMS!
+ * 
+ * Copyright (c) Sergey Romanenko (https://awilum.github.io)
+ *
+ * Licensed under The MIT License.
+ *
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ */
+
+namespace Flextype\Parsers\Shortcodes;
+
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
+
+// Shortcode: [php] php code here [/php]
+parsers()->shortcodes()->addHandler('php', static function (ShortcodeInterface $s) {
+    ob_start();
+    eval($s->getContent());
+    return ob_get_clean();
+});
