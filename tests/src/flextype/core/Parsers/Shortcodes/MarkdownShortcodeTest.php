ad5721288bd01583ffd17b9f95c4df073d01480d
Awilum <sergey.romanenko@flextype.org>
2022-04-12 15:48:51 +0300

    feat(tests): add test for [markdown] shortcode

---
+++ tests/src/flextype/core/Parsers/Shortcodes/MarkdownShortcodeTest.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+test('[markdown] shortcode', function () {
+    $this->assertEquals("<p><strong>Foo</strong></p>\n",
+                        parsers()->shortcodes()->parse('[markdown]**Foo**[/markdown]'));
+});
