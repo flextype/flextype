87b754e437eb8ff6957954da538b8dfb8a85a854
Awilum <sergey.romanenko@flextype.org>
2022-04-12 12:11:00 +0300

    feat(shortcodes): add [markdown] shortcode

---
+++ src/flextype/core/Parsers/Shortcodes/MarkdownShortcode.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+ /**
+ * Flextype - Hybrid Content Management System with the freedom of a headless CMS 
+ * and with the full functionality of a traditional CMS!
+ * 
+ * Copyright (c) Sergey Romanenko (https://awilum.github.io)
+ *
+ * Licensed under The MIT License.
+ *
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ */
+
+namespace Flextype\Parsers\Shortcodes;
+
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
+use function parsers;
+
+// Shortcode: [markdown] markdown text here [/markdown]
+parsers()->shortcodes()->addHandler('markdown', static function (ShortcodeInterface $s) {
+    return parsers()->markdown()->parse($s->getContent());
+});
