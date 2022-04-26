cff9590fa44a0ac8a713cbeddb19e39b47ad826c
Awilum <sergey.romanenko@flextype.org>
2022-04-26 14:22:49 +0300

    feat(fields): use registry item id instead of key

--- src/flextype/core/Entries/Fields/Default/RegistryField.php
+++ src/flextype/core/Entries/Fields/Default/RegistryField.php
@@ -29,7 +29,7 @@ emitter()->addListener('onEntriesFetchSingleHasResult', static function (): void
 
         // Modify fetch.
         foreach (entries()->registry()->get('methods.fetch.result.registry.get') as $field => $body) {
-            $data = collection($data)->merge(collection($data)->set($field, registry()->get($body['key'],
+            $data = collection($data)->merge(collection($data)->set($field, registry()->get($body['id'],
                                                         isset($body['default']) ?
                                                             $body['default'] :
                                                             []))->toArray())->toArray();
