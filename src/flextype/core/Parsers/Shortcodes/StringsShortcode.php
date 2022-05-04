71e53a393b07e9564d1d164cee4f15ac455c1e48
Awilum <sergey.romanenko@flextype.org>
2022-05-04 21:50:18 +0300

    feat(shortcodes): `[strings]` shortcode implementation
    This new shortcode will allow to modify strings for entries fields.

---
+++ src/flextype/core/Parsers/Shortcodes/StringsShortcode.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+ /**
+ * Flextype - Hybrid Content Management System with the freedom of a headless CMS 
+ * and with the full functionality of a traditional CMS!
+ * 
+ * Copyright (c) Sergey Romanenko (https://awilum.github.io)
+ *
+ * Licensed under The MIT License.
+ *
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ */
+
+namespace Flextype\Parsers\Shortcodes;
+
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
+use function parsers;
+use function registry;
+
+// Shortcode: [strings at=1 sort=desc transform=lower,upper]
+parsers()->shortcodes()->addHandler('strings', static function (ShortcodeInterface $s) {
+    if (! registry()->get('flextype.settings.parsers.shortcodes.shortcodes.strings.enabled')) {
+        return '';
+    }
+    
+    $content       = $s->getContent();
+    $varsDelimeter = $s->getParameter('delimeter') ?: '|';
+
+    foreach($s->getParameters() as $key => $value) {
+
+        $values = $value !== null ? strings($value)->contains($varsDelimeter) ? explode($varsDelimeter, $value) : $value : [];
+        
+        if ($key == 'append') {
+            if (is_iterable($values)) {
+                $content = strings($content)->{'append'}(...$values)->toString();
+            } else {
+                $content = strings($content)->{'append'}($values)->toString();
+            }
+        }
+
+        if ($key == 'prepend') {
+            if (is_iterable($values)) {
+                $content = strings($content)->{'prepend'}(...$values)->toString();
+            } else {
+                $content = strings($content)->{'prepend'}($values)->toString();
+            }
+        }
+
+        if ($key == 'after') {
+            $content = strings($content)->{'after'}($values)->toString();
+        }
+
+        if ($key == 'afterLast') {
+            $content = strings($content)->{'afterLast'}($values)->toString();
+        }
+
+        if ($key == 'before') {
+            $content = strings($content)->{'before'}($values)->toString();
+        }
+        
+        if ($key == 'beforeLast') {
+            $content = strings($content)->{'beforeLast'}($values)->toString();
+        }
+
+        if ($key == 'lower') {
+            $content = strings($content)->{'lower'}()->toString();
+        }
+
+        if ($key == 'upper') {
+            $content = strings($content)->{'upper'}()->toString();
+        }
+
+        if ($key == 'sort') {
+            $content = strings($content)->{'wordsSort' . strings($values)->ucfirst()}()->toString();
+        }
+
+        if ($key == 'wordsLimit') {
+            $content = strings($content)->{'wordsLimit'}(isset($values[0]) ? (int) $values[0] : 100, isset($values[1]) ? (string) $values[1] : '...')->toString();
+        }
+    }
+    
+    return (string) $content;
+});
---
+++ tests/src/flextype/core/Parsers/Shortcodes/StringsShortcodeTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+test('[strings] shortcode', function () {    
+    // lower
+    $this->assertEquals("zed foo bar", parsers()->shortcodes()->parse('[strings lower]zed foo bar[/strings]'));
+
+    // upper
+    $this->assertEquals("ZED FOO BAR", parsers()->shortcodes()->parse('[strings upper]zed foo bar[/strings]'));
+
+    // append
+    $this->assertEquals("zed foo bar", parsers()->shortcodes()->parse('[strings append=" bar"]zed foo[/strings]'));
+
+    // prepend
+    $this->assertEquals("zed foo bar", parsers()->shortcodes()->parse('[strings prepend="zed "]foo bar[/strings]'));
+
+    // sort
+    $this->assertEquals("a b c", parsers()->shortcodes()->parse('[strings sort="asc"]b a c[/strings]'));
+    $this->assertEquals("c b a", parsers()->shortcodes()->parse('[strings sort="desc"]b a c[/strings]'));
+
+    // after
+    $this->assertEquals(" bar zed", parsers()->shortcodes()->parse('[strings after="foo"]foo bar zed[/strings]'));
+
+    // afterLast
+    $this->assertEquals(" bar zed", parsers()->shortcodes()->parse('[strings afterLast="foo"]foo foo bar zed[/strings]'));
+
+    // before
+    $this->assertEquals("foo bar ", parsers()->shortcodes()->parse('[strings before="zed"]foo bar zed[/strings]'));
+
+    // beforeLast
+    $this->assertEquals("foo ", parsers()->shortcodes()->parse('[strings beforeLast="foo"]foo foo bar zed[/strings]'));
+
+    // wordsLimit
+    $this->assertEquals("foo...", parsers()->shortcodes()->parse('[strings wordsLimit="1"]foo bar zed[/strings]'));
+    $this->assertEquals("foo >>>", parsers()->shortcodes()->parse('[strings wordsLimit="1| >>>"]foo bar zed[/strings]'));
+});
