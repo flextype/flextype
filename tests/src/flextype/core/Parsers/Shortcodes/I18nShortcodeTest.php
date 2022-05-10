e4eb0e5dc6302de82b39cf7b52eddd74f7834dbe
Awilum <sergey.romanenko@flextype.org>
2022-05-10 19:23:09 +0300

    feat(shortcodes): add `[tr]` shortcode - upd tests

--- tests/src/flextype/core/Parsers/Shortcodes/I18nShortcodeTest.php
+++ tests/src/flextype/core/Parsers/Shortcodes/I18nShortcodeTest.php
@@ -3,5 +3,5 @@
 declare(strict_types=1);
 
 test('[tr] shortcode', function () {
-   expect(parsers()->shortcodes()->parse('[tr find="site_title"]'))->toBe('Site');
+   expect(parsers()->shortcodes()->parse('[tr find="Foo"]'))->toBe('Foo');
 });
