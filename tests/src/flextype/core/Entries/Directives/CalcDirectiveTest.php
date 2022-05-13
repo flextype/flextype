8e050eefbee19b283f2aaf01757b6899a540912c
Awilum <sergey.romanenko@flextype.org>
2022-05-13 20:50:39 +0300

    feat(directives): add '@calc' directive

---
+++ tests/src/flextype/core/Entries/Directives/CalcDirectiveTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Flextype\Component\Filesystem\Filesystem;
+
+beforeEach(function() {
+    filesystem()->directory(PATH['project'] . '/entries')->create();
+});
+
+afterEach(function (): void {
+    filesystem()->directory(PATH['project'] . '/entries')->delete();
+});
+
+test('calc directive', function () {
+    entries()->create('field', ['foo' => '@calc(2+2)']);
+    $this->assertEquals(4, entries()->fetch('field')['foo']);
+});
