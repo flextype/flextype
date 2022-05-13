c1c9bcde7538c2dcf2122c0ca9fbc38f2a40e979
Awilum <sergey.romanenko@flextype.org>
2022-05-13 15:27:17 +0300

    feat(directives): add `vars` directive

---
+++ src/flextype/core/Entries/Directives/VarsDirective.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+ /**
+ * Flextype - Hybrid Content Management System with the freedom of a headless CMS 
+ * and with the full functionality of a traditional CMS!
+ * 
+ * Copyright (c) Sergey Romanenko (https://awilum.github.io)
+ *
+ * Licensed under The MIT License.
+ *
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ */
+
+use Glowy\Arrays\Arrays as Collection;
+
+emitter()->addListener('onEntriesFetchSingleField', static function (): void {
+
+    if (! registry()->get('flextype.settings.entries.directives.vars.enabled')) {
+        return;
+    }
+    
+    $field  = entries()->registry()->get('methods.fetch.field');
+    $result = entries()->registry()->get('methods.fetch.result');
+
+    if (is_string($field['value'])) {
+        $field['value'] = preg_replace_callback('/@var\((.*?)\)/', function($matches) use ($result) {
+            return collection($result['vars'])->get($matches[1]);
+        }, $field['value']);
+    }
+
+    entries()->registry()->set('methods.fetch.field.key', $field['key']);
+    entries()->registry()->set('methods.fetch.field.value', $field['value']);
+});
---
+++ tests/src/flextype/core/Entries/Directives/VarsDirectiveTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use Flextype\Component\Filesystem\Filesystem;
+
+beforeEach(function() {
+    filesystem()->directory(PATH['project'] . '/entries')->create();
+});
+
+afterEach(function (): void {
+    filesystem()->directory(PATH['project'] . '/entries')->delete();
+});
+
+test('vars directive', function () {
+    entries()->create('type-vars', ['vars' => ['foo' => 'Foo'], 'title' => '@var(foo)']);
+
+    $this->assertEquals('Foo', entries()->fetch('type-vars')['title']);
+});
