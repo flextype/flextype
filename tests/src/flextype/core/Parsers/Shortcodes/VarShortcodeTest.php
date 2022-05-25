35912b84a0f439e35c51054491624d2af47ebcbd
Awilum <sergey.romanenko@flextype.org>
2022-05-25 18:01:09 +0300

    feat(tests): add missed tests for `var` shortcode

---
+++ tests/src/flextype/core/Parsers/Shortcodes/VarShortcodeTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+beforeEach(function() {
+    filesystem()->directory(PATH['project'] . '/entries')->ensureExists(0755, true);
+});
+
+afterEach(function () {
+    filesystem()->directory(PATH['project'] . '/entries')->delete();
+});
+
+test('var shortcode', function () {
+    expect(entries()->create('foo', ['var' => ['foo' => 'Foo'], 'title' => '(var:foo)']))->toBeTrue();
+    expect(entries()->fetch('foo')['title'])->toBe('Foo');
+});
