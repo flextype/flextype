b3eaac60aa606914cf814f4dcc7a8552ac843930
Awilum <sergey.romanenko@flextype.org>
2022-05-26 20:18:56 +0300

    feat(shortcodes): add missed settings

--- tests/src/flextype/core/Parsers/Shortcodes/RegistryShortcodeTest.php
+++ tests/src/flextype/core/Parsers/Shortcodes/RegistryShortcodeTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-test('[registry] shortcode', function () {
+test('registry shortcode', function () {
     expect(strings(parsers()->shortcodes()->parse("(registry get:'flextype.manifest')"))->isJson())->toBeTrue();
     expect(parsers()->shortcodes()->parse("(registry get:'flextype.manifest.name')"))->toBe('Flextype');
     expect(parsers()->shortcodes()->parse("(registry get:'flextype.manifest.foo' default:'Default')"))->toBe('Default');
