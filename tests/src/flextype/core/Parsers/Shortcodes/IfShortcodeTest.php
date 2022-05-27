bc5477bd57a8fb154316cf3b6337ff5e69cd7f6f
Awilum <sergey.romanenko@flextype.org>
2022-05-27 08:59:04 +0300

    feat(shortcodes): update logic for `if` shortcode, use expression service

--- tests/src/flextype/core/Parsers/Shortcodes/IfShortcodeTest.php
+++ tests/src/flextype/core/Parsers/Shortcodes/IfShortcodeTest.php
@@ -2,27 +2,8 @@
 
 declare(strict_types=1);
 
-test('(if] shortcode', function () {
-    expect(parsers()->shortcodes()->parse("(if val1:'2' operator:'lt' val2:'5')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'2' operator:'<' val2:'5')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'5' operator:'gt' val2:'2')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'2' operator:'lte' val2:'5')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'2' operator:'<=' val2:'5')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'5' operator:'gte' val2:'2')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'5' operator:'>=' val2:'2')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'5' operator:'eq' val2:'5')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'5' operator:'=' val2:'5')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'2' operator:'neq' val2:'5')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'2' operator:'<>' val2:'5')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'2' operator:'!=' val2:'5')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'foobarfoo' operator:'contains' val2:'bar')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'foobarfoo' operator:'like' val2:'bar')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'foobarfoo' operator:'ncontains' val2:'zed')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'foobarfoo' operator:'nlike' val2:'zed')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'foobar' operator:'starts_with' val2:'foo')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'foobar' operator:'ends_with' val2:'bar')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'2020-11-12' operator:'newer' val2:'2020-11-11')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'2020-11-11' operator:'older' val2:'2020-11-12')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'PHP is the web scripting language of choice.' operator:'regexp' val2:'php')yes(/if)"))->toBe("yes");
-    expect(parsers()->shortcodes()->parse("(if val1:'PHP is the web scripting language of choice.' operator:'nregexp' val2:'delphi')yes(/if)"))->toBe("yes");
+test('if shortcode', function () {
+    expect(parsers()->shortcodes()->parse("(if:'2 > 1')yes(/if)"))->toBe("yes");
+    expect(parsers()->shortcodes()->parse("(if:'2>1')yes(/if)"))->toBe("yes");
+    expect(parsers()->shortcodes()->parse("(if:'2<1')yes(/if)"))->toBe("");
 });
