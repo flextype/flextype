457563dca05256cf8682ed7bb38a084cd81073c1
Awilum <sergey.romanenko@flextype.org>
2022-05-31 19:58:55 +0300

    feat(tests): improve tests for expressions directive

--- tests/src/flextype/core/Entries/Directives/ExpressionsDirectiveTest.php
+++ tests/src/flextype/core/Entries/Directives/ExpressionsDirectiveTest.php
@@ -13,11 +13,15 @@ afterEach(function (): void {
 test('expressions directive', function () {
     entries()->create('expressions', ['test' => '[[ 1+1 ]]']);
     $this->assertEquals(2, entries()->fetch('expressions')['test']);
+});
 
+test('expressions directive disabled', function () {
     registry()->set('flextype.settings.entries.directives.expressions.enabled', false);
     expect(entries()->fetch('expressions')['test'])->toBe('[[ 1+1 ]]');
     registry()->set('flextype.settings.entries.directives.expressions.enabled', true);
+});
 
+test('expressions directive disabled globaly', function () {
     registry()->set('flextype.settings.entries.directives.expressions.enabled_globaly', false);
     expect(entries()->fetch('expressions')['test'])->toBe('[[ 1+1 ]]');
     registry()->set('flextype.settings.entries.directives.expressions.enabled_globaly', true);
