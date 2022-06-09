daaa3ee1efde812da9b2ebf34bb3bff77b255d96
Awilum <sergey.romanenko@flextype.org>
2022-06-09 20:16:47 +0300

    feat(tests): new constants `PATH_PROJECT` and `PATH_TMP` instead of old c…

--- tests/Pest.php
+++ tests/Pest.php
@@ -5,10 +5,8 @@ declare(strict_types=1);
 namespace Flextype;
 
 define('ROOT_DIR', str_replace(DIRECTORY_SEPARATOR, '/', getcwd()));
-define('PATH', [
-    'project'   => ROOT_DIR . '/project',
-    'tmp'   => ROOT_DIR . '/var/tmp',
-]);
+define('PATH_PROJECT', ROOT_DIR . '/project');
+define('PATH_TMP', ROOT_DIR . '/var/tmp');
 
 ! is_file($flextype_autoload = ROOT_DIR . '/vendor/autoload.php') and exit('Please run: <i>composer install</i> for flextype');
 $flextype_loader = require_once $flextype_autoload;
