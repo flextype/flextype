8b12a975832caf2c2a7ece60c6de501f0d1dca52
Awilum <sergey.romanenko@flextype.org>
2022-07-07 17:54:06 +0300

    refactor(core): add missed copyrights headers

--- src/flextype/helpers/finder.php
+++ src/flextype/helpers/finder.php
@@ -2,6 +2,18 @@
 
 declare(strict_types=1);
 
+ /**
+ * Flextype - Hybrid Content Management System with the freedom of a headless CMS
+ * and with the full functionality of a traditional CMS!
+ *
+ * Copyright (c) Sergey Romanenko (https://awilum.github.io)
+ *
+ * Licensed under The MIT License.
+ *
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ */
+
 namespace Flextype;
 
 use Symfony\Component\Finder\Finder;
