a13fe8dd558a528cf3df74efbe05bbdaf9d5d8d4
Awilum <sergey.romanenko@flextype.org>
2022-08-28 20:04:49 +0300

    feat(tests): improve tests for entries expressions

--- tests/src/flextype/core/Entries/Expressions/EntriesExpressionTest.php
+++ tests/src/flextype/core/Entries/Expressions/EntriesExpressionTest.php
@@ -14,6 +14,10 @@ afterEach(function (): void {
 
 test('entries expression', function () {
     entries()->create('foo', ['title' => 'Foo']);
-    entries()->create('entries', ['test' => '[[ entries().fetch("foo").get("title") ]]']);
+    entries()->create('entries', ['test' => '[[ entries().fetch("foo").get("title") ]]', 
+                                  'test2' => '(type:bool) [[ entries().has("foo") ]]',
+                                  'test3' => '(type:bool) [[ entries().has("bar") ]]']);
     expect(entries()->fetch('entries')['test'])->toBe('Foo');
+    expect(entries()->fetch('entries')['test2'])->toBe(true);
+    expect(entries()->fetch('entries')['test3'])->toBe(false);
 });
